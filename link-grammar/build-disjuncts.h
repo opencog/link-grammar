5e519757b7d270b70c3f7f30f57271a6cc8dd2ad
Dom Lachowicz <domlachowicz@gmail.com>
2006-06-30 12:07:30 +0000

    fix build bug martin uncovered
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/link-grammar/trunk@22563 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- link-grammar/build-disjuncts.h
+++ link-grammar/build-disjuncts.h
@@ -10,8 +10,6 @@
 /* forms, with or without modification, subject to certain conditions.          */
 /*                                                                              */
 /********************************************************************************/
-int  maxcost_of_sentence(Sentence sent);
 void build_sentence_disjuncts(Sentence sent, int cost_cutoff);
-void print_disjunct_list(Disjunct *);
 X_node *   build_word_expressions(Sentence sent, char *);
 Disjunct * build_disjuncts_for_dict_node(Dict_node *);
