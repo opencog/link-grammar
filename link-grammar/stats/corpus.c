c3a46ac75f3778577033bc3787852340e6a5f908
Linas Vepstas <linasvepstas@gmail.com>
2008-12-18 01:59:01 +0000

    start roughing in the actual corpus api
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/link-grammar/trunk@25216 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ link-grammar/stats/corpus.c
@@ -0,0 +1,50 @@
+/*
+ * corpus.c
+ *
+ * Data for corpus statistics, used to provide a parse ranking
+ * to drive the SAT solver, as well as parse ranking with the
+ * ordinary solver. 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+#include "corpus.h"
+
+Corpus * lg_corpus_new(void)
+{
+	int rc;
+
+	Corpus *c = (Corpus *) malloc(sizeof(Corpus));
+	c->errmsg = NULL;
+
+#if LATER
+	c->dbname = "/home/linas/src/novamente/src/link-grammar/trunk/data/sql/disjuncts.db";
+
+	rc = sqlite3_open(dbname, &c->dbconn);
+	if (rc)
+	{
+		prt_err("Warning: Can't open database: %s\n", sqlite3_errmsg(c->dbconn));
+		sqlite3_close(c->dbconn);
+		c->dbconn = NULL;
+	}
+#endif
+
+	return c;
+}
+
+void lg_corpus_delete(Corpus *c)
+{
+	if (c->dbconn)
+	{
+		sqlite3_close(c->dbconn);
+		c->dbconn = NULL;
+	}
+	free(c);
+}
+
+double lg_corpus_score(Corpus *corp, Linkage *linakge)
+{
+printf ("hello world\n");
+}
+
---
+++ link-grammar/stats/corpus.h
@@ -0,0 +1,28 @@
+/*
+ * corpus.h
+ *
+ * Data for corpus statistics, used to provide a parse ranking
+ * to drive the SAT solver, as well as parse ranking with the
+ * ordinary solver. 
+ */
+
+#ifndef _LINKGRAMMAR_CORPUS_H
+#define _LINKGRAMMAR_CORPUS_H
+
+#include <sqlite3.h>
+#include "../link-includes.h"
+
+struct corpus_s
+{
+	char * dbname;
+	sqlite3 *dbconn;
+	char *errmsg;
+};
+
+typedef struct corpus_s Corpus;
+
+Corpus * lg_corpus_new(void);
+void lg_corpus_delete(Corpus *);
+double lg_corpus_score(Corpus *, Linkage *);
+
+#endif /* _LINKGRAMMAR_CORPUS_H */
