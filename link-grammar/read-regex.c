86a8f0b65e6928d700ae6b53fe97128b5ef08f35
Linas Vepstas <linasvepstas@gmail.com>
2009-03-29 03:19:03 +0000

    whitespace hacking
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/link-grammar/trunk@25950 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- link-grammar/read-regex.c
+++ link-grammar/read-regex.c
@@ -1,14 +1,13 @@
-/*****************************************************************************/
-/* Copyright (c) 2005                                                        */
-/* Sampo Pyysalo                                                             */
-/*                                                                           */
-/* Use of the link grammar parsing system is subject to the terms of the     */
-/* license set forth in the LICENSE file included with this software,        */
-/* and also available at http://www.link.cs.cmu.edu/link/license.html        */
-/* This license allows free redistribution and use in source and binary      */
-/* forms, with or without modification, subject to certain conditions.       */
-/*                                                                           */
-/*****************************************************************************/
+/*************************************************************************/
+/* Copyright (c) 2005 Sampo Pyysalo                                      */
+/*                                                                       */
+/* Use of the link grammar parsing system is subject to the terms of the */
+/* license set forth in the LICENSE file included with this software,    */
+/* and also available at http://www.link.cs.cmu.edu/link/license.html    */
+/* This license allows free redistribution and use in source and binary  */
+/* forms, with or without modification, subject to certain conditions.   */
+/*                                                                       */
+/*************************************************************************/
 
 #include "link-includes.h"
 
@@ -37,100 +36,100 @@
 #define MAX_REGEX_NAME_LENGTH 50
 #define MAX_REGEX_LENGTH      255
 
-int read_regex_file(char *regex_path_name, char *regex_file_name,
-		    Dictionary dict) {
-  Regex_node **tail = &dict->regex_root; /* Last Regex_node * in list */
-  Regex_node *new_re;
-  char name[MAX_REGEX_NAME_LENGTH];
-  char regex[MAX_REGEX_LENGTH];
-  int c,prev,i,line=1;
-  FILE *fp;
-  
-  /* use dictopen to apply same path search as for the dictionary. */
-  fp = dictopen(regex_path_name, regex_file_name, "r");
-  if(fp == NULL) {
-    lperror(NODICT, regex_file_name);
-    return 0;
-  }
+int read_regex_file(Dictionary dict, char *file_name)
+{
+	Regex_node **tail = &dict->regex_root; /* Last Regex_node * in list */
+	Regex_node *new_re;
+	char name[MAX_REGEX_NAME_LENGTH];
+	char regex[MAX_REGEX_LENGTH];
+	int c,prev,i,line=1;
+	FILE *fp;
+	
+	/* use dictopen to apply same path search as for the dictionary. */
+	fp = dictopen(regex_path_name, regex_file_name, "r");
+	if(fp == NULL) {
+		lperror(NODICT, regex_file_name);
+		return 0;
+	}
 
-  /* read in regexs. loop broken on EOF. */
-  for(;;) {
-    /* skip whitespace and comments. */
-    do {
-      do { 
-	c=fgetc(fp);
-	if(c == '\n') { line++; }
-      } while(isspace(c));
-      if(c == '%') {
-	while((c != EOF) && (c != '\n')) { c=fgetc(fp); }
-	line++;
-      }
-    } while(isspace(c));
+	/* read in regexs. loop broken on EOF. */
+	for(;;) {
+		/* skip whitespace and comments. */
+		do {
+			do { 
+				c=fgetc(fp);
+				if(c == '\n') { line++; }
+			} while(isspace(c));
+			if(c == '%') {
+				while((c != EOF) && (c != '\n')) { c=fgetc(fp); }
+				line++;
+			}
+		} while(isspace(c));
 
-    if(c == EOF) { break; } /* done. */
+		if(c == EOF) { break; } /* done. */
 
-    /* read in the name of the regex. */
-    i = 0;
-    do {
-      if(i > MAX_REGEX_NAME_LENGTH-1) {
-	lperror(DICTPARSE, ". Regex name too long on line %d", line);
-	return 0;
-      }
-      name[i++] = c;
-      c=getc(fp);
-    } while((!isspace(c)) && (c != ':') && (c != EOF));
-    name[i] = '\0';
-    
-    /* skip possible whitespace after name, expect colon. */
-    while(isspace(c)) { 
-      if(c == '\n') { line++; }
-      c=getc(fp); 
-    }
-    if(c != ':') {
-      lperror(DICTPARSE, ". Regex  missing colon on line %d", line);
-      return 0;
-    }
+		/* read in the name of the regex. */
+		i = 0;
+		do {
+			if(i > MAX_REGEX_NAME_LENGTH-1) {
+				lperror(DICTPARSE, ". Regex name too long on line %d", line);
+				return 0;
+			}
+			name[i++] = c;
+			c=getc(fp);
+		} while((!isspace(c)) && (c != ':') && (c != EOF));
+		name[i] = '\0';
+		
+		/* skip possible whitespace after name, expect colon. */
+		while(isspace(c)) { 
+			if(c == '\n') { line++; }
+			c=getc(fp); 
+		}
+		if(c != ':') {
+			lperror(DICTPARSE, ". Regex	missing colon on line %d", line);
+			return 0;
+		}
 
-    /* skip whitespace after colon, expect slash. */
-    do {
-      if(c == '\n') { line++; }
-      c=getc(fp); 
-    } while(isspace(c));
-    if(c != '/') {
-      lperror(DICTPARSE, ". Regex  missing slash on line %d\n", line);
-      return 0;      
-    }
+		/* skip whitespace after colon, expect slash. */
+		do {
+			if(c == '\n') { line++; }
+			c=getc(fp); 
+		} while(isspace(c));
+		if(c != '/') {
+			lperror(DICTPARSE, ". Regex	missing slash on line %d\n", line);
+			return 0;			
+		}
 
-    /* read in the regex. */
-    prev = 0;
-    i = 0;
-    do {
-      if(i > MAX_REGEX_LENGTH-1) {
-	lperror(DICTPARSE, ". Regex too long on line %d", line);
-	return 0;
-      }
-      prev=c;
-      c=getc(fp);
-      regex[i++] = c;
-    } while((c != '/' || prev == '\\') && (c != EOF));
-    regex[i-1] = '\0';
+		/* read in the regex. */
+		prev = 0;
+		i = 0;
+		do {
+			if(i > MAX_REGEX_LENGTH-1) {
+				lperror(DICTPARSE, ". Regex too long on line %d", line);
+				return 0;
+			}
+			prev=c;
+			c=getc(fp);
+			regex[i++] = c;
+		} while((c != '/' || prev == '\\') && (c != EOF));
+		regex[i-1] = '\0';
 
-    /* expect termination by a slash. */
-    if(c != '/') {
-      lperror(DICTPARSE, ". Regex  missing slash on line %d", line);
-      return 0;      
-    }
+		/* expect termination by a slash. */
+		if(c != '/') {
+			lperror(DICTPARSE, ". Regex	missing slash on line %d", line);
+			return 0;			
+		}
 
-    /* Create new Regex_node and add to dict list. */
-    new_re = (Regex_node *)malloc(sizeof(Regex_node));
-    new_re->name    = strdup(name);
-    new_re->pattern = strdup(regex);
-    new_re->re      = NULL;
-    new_re->next    = NULL;
-    *tail = new_re;
-    tail  = &new_re->next;
-  }
-  fclose(fp);
-  return 1;
+		/* Create new Regex_node and add to dict list. */
+		new_re = (Regex_node *)malloc(sizeof(Regex_node));
+		new_re->name		= strdup(name);
+		new_re->pattern = strdup(regex);
+		new_re->re			= NULL;
+		new_re->next		= NULL;
+		*tail = new_re;
+		tail	= &new_re->next;
+	}
+	fclose(fp);
+	return 1;
 }
 
