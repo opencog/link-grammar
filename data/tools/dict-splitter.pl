ca9b257c26037b206895e6d7777afdc41ef84d3d
Linas Vepstas <linasvepstas@gmail.com>
2009-08-09 23:52:42 +0000

    seem to have an operational splitter now
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/link-grammar/trunk@27562 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- data/tools/dict-splitter.pl
+++ data/tools/dict-splitter.pl
@@ -7,6 +7,7 @@
 
 $fileno = 0;
 $doprt = 1;
+$didprt = 0;
 while(<>)
 {
 	chop;
@@ -17,32 +18,48 @@ while(<>)
 	{
 		
 		($wds, $rules) = split /:/;
-		print "duude $wds\n";
+
+		if (($wds =~ /^<.*>$/)  ||
+		    ($wds =~ /^\//))
+		{} else
+		{
+			print "$wds\n";
+			$didprt = 1;
+		}
 
 		$doprt = 0;
 		if ($rules =~ /;/)
 		{
 			$doprt = 1;
-			$fileno ++;
-
-			print "--------------------- $fileno\n";
+			if ($didprt)
+			{
+				$fileno ++;
+				print "--------------------- $fileno\n";
+			}
+			$didprt = 0;
 		}
 		next;
 	}
 
 	if ($doprt)
 	{
-		if (/^\s*$/) { next; }
-		print "whole line $_\n";
+		if (/^\s*$/) { next; }   # if all whitespace, skip
+		if (/^\//) { next; }
+		print "$_\n";
+		$didprt = 1;
 		next;
 	}
 	
 	if (/;/)
 	{
 		$doprt = 1;
-		$fileno ++;
 
-		print "--------------------- $fileno\n";
+		if ($didprt)
+		{
+			$fileno ++;
+			print "--------------------- $fileno\n";
+		}
+		$didprt = 0;
 	}
 
 }
