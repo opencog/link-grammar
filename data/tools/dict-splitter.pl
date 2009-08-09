8d2d787e090ee16833ebcfea19b664c828044744
Linas Vepstas <linasvepstas@gmail.com>
2009-08-09 23:36:23 +0000

    more code for the doc-splitter
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/link-grammar/trunk@27561 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- data/tools/dict-splitter.pl
+++ data/tools/dict-splitter.pl
@@ -4,7 +4,45 @@
 #
 # Split dictionary into distinct files.
 #
+
+$fileno = 0;
+$doprt = 1;
 while(<>)
 {
+	chop;
+	if (/^%/) { next; }
+	if (/^ %/) { next; }
+
+	if (/:/)
+	{
+		
+		($wds, $rules) = split /:/;
+		print "duude $wds\n";
+
+		$doprt = 0;
+		if ($rules =~ /;/)
+		{
+			$doprt = 1;
+			$fileno ++;
+
+			print "--------------------- $fileno\n";
+		}
+		next;
+	}
+
+	if ($doprt)
+	{
+		if (/^\s*$/) { next; }
+		print "whole line $_\n";
+		next;
+	}
 	
+	if (/;/)
+	{
+		$doprt = 1;
+		$fileno ++;
+
+		print "--------------------- $fileno\n";
+	}
+
 }
