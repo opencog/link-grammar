56d4b57372d99e2c945ca9042d8710e91381d7cd
Linas Vepstas <linasvepstas@gmail.com>
2009-08-13 21:44:37 +0000

    scripts to populate the clusters table.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/link-grammar/trunk@27650 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- data/tools/cluster-pop.pl
+++ data/tools/cluster-pop.pl
@@ -5,25 +5,46 @@
 # Populate the cluster table from Siva's clusters.
 # Siva's clusters seems to contain just 13506 word forms ... 
 #
+# Usage: cat clusters_weka-trimmed.txt | ./cluster-pop.pl
+#
+
+use DBI;
 
 $| = 1;
 
-$fileprev = "";
-$cnt = 0;
+my $dbh = DBI->connect('DBI:SQLite:dbname=clusters.db', 'linas', 'asdf')
+   or die "Couldn't connect to database: " . DBI->errstr;
+
+my $insert = $dbh->prepare(
+	'INSERT INTO ClusterMembers (cluster_name, inflected_word) VALUES (?,?)');
+
+my $cluster_name = "";
+my $inflected_word = "";
+
+my $clust_cnt = 0;
+my $word_cnt = 0;
+
 while(<>)
 {
 	chop;
 
 	if (/^cluster/)
 	{
-		print "$cnt\n";
-		print "$_\n";
-		$cnt = 0;
+		$cluster_name = $_;
+		print "$cluster_name\n";
+		$clust_cnt ++;
 		next;
 	}
 	if (/^	(.*)$/)
 	{
-		$wrd = $1;
-print "duude $wrd";
+		$inflected_word = $1;
+		print "\t$inflected_word\n";
+		$insert->execute($cluster_name, $inflected_word)
+			or die "Couldn't execute statement: " . $insert->errstr . 
+			"\nword was " . $inflected_word;
+
+		$word_cnt ++;
 	}
 }
+
+print "Added $word_cnt words to $clust_cnt clusters\n";
