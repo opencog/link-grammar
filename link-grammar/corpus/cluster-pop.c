7566af5e798740ecfbc6ed34c46b0efb6fad3bab
Linas Vepstas <linasvepstas@gmail.com>
2009-08-14 00:08:32 +0000

    more of the cluster-populate implementation
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/link-grammar/trunk@27654 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- link-grammar/corpus/cluster-pop.c
+++ link-grammar/corpus/cluster-pop.c
@@ -20,6 +20,7 @@ int main (int argc, char * argv[])
 
 	const char * dbname = "../../data/sql/clusters.db";
 
+	/* Open the database for update */
 	int rc = sqlite3_open_v2(dbname, &dbconn, SQLITE_OPEN_READWRITE, NULL);
 	if (rc)
 	{
@@ -40,6 +41,19 @@ int main (int argc, char * argv[])
 		exit(1);
 	}
 
+	sqlite3_stmt *cluword_query;
+	rc = sqlite3_prepare_v2(dbconn,
+		"SELECT inflected_word FROM ClusterMembers WHERE cluster_name = ?;",
+		-1, &cluword_query, NULL);
+
+	if (rc != SQLITE_OK)
+	{
+		fprintf(stderr, "Error: cannot prepare first stmt\n");
+		sqlite3_close(dbconn);
+		exit(1);
+	}
+
+	/* Loop over all cluster names */
 	while(1)
 	{
 		rc = sqlite3_step(cluname_query);
@@ -48,7 +62,19 @@ int main (int argc, char * argv[])
 		const char * cluname = sqlite3_column_text(cluname_query,0);
 
 printf("got on %s\n", cluname);
+		rc = sqlite3_bind_text(cluword_query, 1, cluname, -1, SQLITE_STATIC);
+		while(1)
+		{
+			rc = sqlite3_step(cluword_query);
+			if (rc != SQLITE_ROW) break;
+			const char * cluword = sqlite3_column_text(cluword_query,0);
+printf("\tgot wrd %s\n", cluword);
+		}
+		sqlite3_reset(cluword_query);
+		sqlite3_clear_bindings(cluword_query);
 	}
+	sqlite3_reset(cluname_query);
+	sqlite3_clear_bindings(cluname_query);
 
 	sqlite3_close(dbconn);
 	return 0;
