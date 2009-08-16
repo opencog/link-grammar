ea51a1ca7db015de97db21bc2790b6f6b49d3571
Linas Vepstas <linasvepstas@gmail.com>
2009-08-16 21:57:25 +0000

    move assorted musical instruments from words.n.1 to words.n.4
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/link-grammar/trunk@27703 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ data/tools/delete.pl
@@ -0,0 +1,62 @@
+#! /usr/bin/env perl
+#
+# delete.pl
+#
+# Dictionary maintenance tool - delete words from dictionary.
+# Given a list of words and a dictionary file, this script
+# will delete the words from the file.
+#
+# It is assumed that the word-list holds one word per line,
+# and that the word-list is already in alphabetical order.
+#
+# Example usage:
+# ./delete.pl wordlist_file en/words/words.adj.1
+#
+# Copyright (C) 2009 Linas Vepstas <linasvepstas@gmail.com>
+#
+
+if ($#ARGV < 1)
+{
+	die "Usage: insert.pl wordlist-file dict-file\n";
+}
+
+# Get the word-list, and the dictionary, from the command line
+my $wordlist_file = @ARGV[0];
+my $dict_file = @ARGV[1];
+
+open(WORDS, $wordlist_file);
+my @words = ();
+while (<WORDS>)
+{
+	# Get rid of newline at end of word.
+	chop;
+	push @words, $_;
+}
+close(WORDS);
+
+my $word = shift @words;
+
+open (DICT, $dict_file);
+while (<DICT>)
+{
+	chop;
+	my @entries = split;
+
+	# Loop over the entries
+	foreach (@entries)
+	{
+		my $wd = $_;
+		if (($_ ne $word) && ($word ne ""))
+		{
+			print "$_ ";
+		}
+		else
+		{
+			$word = shift @words;
+		}
+	}
+	print "\n";
+}
+close (DICT);
+
+
