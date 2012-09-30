3dccc85d54da6c9a0cb82f7fd591e259037bb591
Linas Vepstas <linasvepstas@gmail.com>
2012-09-30 15:23:30 +0000

    All-new, all-fixed up algo
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/link-grammar/trunk@31985 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- viterbi/state.cc
+++ viterbi/state.cc
@@ -24,7 +24,6 @@ using namespace std;
 
 namespace link_grammar {
 namespace viterbi {
-int cnt = 0;
 
 /* Current parse state */
 Atom * Parser::lg_exp_to_atom(Exp* exp, bool more)
@@ -34,99 +33,34 @@ Atom * Parser::lg_exp_to_atom(Exp* exp, bool more)
 		stringstream ss;
 		if (exp->multi) ss << "@";
 		ss << exp->u.string << exp->dir;
-		Node *n = new Node(CONNECTOR, ss.str());
-for(int i=0; i<cnt; i++) cout <<"  ";
-cout<<"duude n="<<exp->u.string<<endl;
-		return n;
+		return new Node(CONNECTOR, ss.str());
 	}
 
+	// Whenever a null appears in an OR-list, it means the 
+	// entire OR-list is optional.  A null can never appear
+	// in an AND-list.
 	E_list* el = exp->u.l;
 	if (NULL == el)
-	{
-		stringstream ss;
-		ss << "()";
-		Node *n = new Node(CONNECTOR, ss.str());
-for(int i=0; i<cnt; i++) cout <<"  ";
-cout<<"duude n= null ()"<<endl;
-		return n;
-	}
+		return new Node(CONNECTOR, "0");
 
-for(int i=0; i<cnt; i++) cout <<"  ";
-cout<<"exp=";
-print_expression(exp);
-
-	/* Handle optional expressions */
-	// if (NULL == el->e->u.l)
-	if ((OR_type == exp->type) and (NULL == el->e->u.l))
-	{
-for(int i=0; i<cnt; i++) cout <<"  ";
-cout<<"duude n= opt "<<endl;
-cnt++;
-		el = el->next;
-		Atom* a = lg_exp_to_atom(el->e);
-cnt--;
-		OutList oset;
-		oset.push_back(a);
-		Link* l = new Link(OPTIONAL, oset);
-		return l;
-	}
-
-if (AND_type == exp->type)  {
-for(int i=0; i<cnt; i++) cout <<"  ";
-cout<<"duude AND "<<endl; }
-else if (OR_type == exp->type){
-for(int i=0; i<cnt; i++) cout <<"  ";
-cout<<"duude OR "<<endl;  }
-else cout<<"duude wtf not either!"<<endl;
-
+	// The data structure that link-grammar uses for connector
+	// expressions is totally insane, as witnessed by the loop below.
+	// Anyway: operators are infixed, i.e. are always binary,
+	// with exp->u.l->e being the left hand side, and
+	//      exp->u.l->next->e being the right hand side.
+	// This means that exp->u.l->next->next is always null.
 	OutList alist;
-
-assert(el != NULL, "unhandled null here");
-	// operators are infixed, i.e. are always binary.
-	// That is, for each type, theres an exper on the left, and on the right.
-cnt++;
-	Atom* a = lg_exp_to_atom(el->e);
-cnt--;
-	alist.push_back(a);
+	alist.push_back(lg_exp_to_atom(el->e));
 	el = el->next;
 
 	while (exp->type == el->e->type)
 	{
 		el = el->e->u.l;
-		if (el->e->u.l == NULL)
-		{
-for(int i=0; i<cnt; i++) cout <<"  ";
-cout<<"duude loopy oooopt "<<endl;
-			break;
-		}
-cnt++;
-		Atom* a = lg_exp_to_atom(el->e);
-cnt--;
-		alist.push_back(a);
+		alist.push_back(lg_exp_to_atom(el->e));
 		el = el->next;
 	}
-for(int i=0; i<cnt; i++) cout <<"  ";
-cout<<"duude final "<<endl;
 
-	if (el->e->u.l == NULL)
-	{
-for(int i=0; i<cnt; i++) cout <<"  ";
-cout<<"duude final opt oooopt "<<endl;
-cnt++;
-		el = el->next;
-		a = lg_exp_to_atom(el->e);
-cnt--;
-		OutList oset;
-		oset.push_back(a);
-		a = new Link(OPTIONAL, oset);
-	}
-	else
-	{
-cnt++;
-		a = lg_exp_to_atom(el->e);
-cnt--;
-	}
-	alist.push_back(a);
+	alist.push_back(lg_exp_to_atom(el->e));
 
 	if (AND_type == exp->type)
 		return new Link(AND, alist);
@@ -156,6 +90,7 @@ print_expression(exp);
 	dlist.push_back(dj);
 
 	Link *wdj = new Link(WORD_DISJ, dlist);
+cout <<"duuuude word disj=\n"<< wdj <<endl;
 	return wdj;
 }
 
@@ -170,9 +105,6 @@ void Parser::initialize_state()
 
 	_state = new Link(STATE, statev);
 
-	//cout <<"duuuude state="<< _state <<endl;
-Atom *a=word_disjuncts("XXX");
-cout<<a<<endl;
 }
 
 void viterbi_parse(Dictionary dict, const char * sentence)
@@ -181,9 +113,8 @@ void viterbi_parse(Dictionary dict, const char * sentence)
 
 	// Initial state
 	pars.initialize_state();
-cout <<"Hello world!"<<endl;
 
-//	pars.word_disjuncts("sing");
+pars.word_disjuncts("sing");
 }
 
 } // namespace viterbi
