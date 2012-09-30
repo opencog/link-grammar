1cc23aa94c63273582d152bf288c508f7e5114a0
Linas Vepstas <linasvepstas@gmail.com>
2012-09-30 16:26:14 +0000

    remove debug prints, now it works.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/link-grammar/trunk@31987 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- viterbi/state.cc
+++ viterbi/state.cc
@@ -24,19 +24,15 @@ using namespace std;
 
 namespace link_grammar {
 namespace viterbi {
-int cnt = 0;
 
 /* Current parse state */
 Atom * Parser::lg_exp_to_atom(Exp* exp, bool more)
 {
-assert(exp, "wtf bad arg");
 	if (CONNECTOR_type == exp->type)
 	{
 		stringstream ss;
 		if (exp->multi) ss << "@";
 		ss << exp->u.string << exp->dir;
-for(int i=0; i<cnt; i++) cout <<"  ";
-cout<<"duude n="<<ss<<endl;
 		return new Node(CONNECTOR, ss.str());
 	}
 
@@ -45,56 +41,25 @@ cout<<"duude n="<<ss<<endl;
 	// in an AND-list.
 	E_list* el = exp->u.l;
 	if (NULL == el)
-	{
-for(int i=0; i<cnt; i++) cout <<"  ";
-cout<<"duude n= null ()"<<endl;
 		return new Node(CONNECTOR, "0");
-	}
 
-for(int i=0; i<cnt; i++) cout <<"  ";
-cout<<"exp=";
-print_expression(exp);
-
-if (AND_type == exp->type)  {
-for(int i=0; i<cnt; i++) cout <<"  ";
-cout<<"duude AND "<<endl; }
-else if (OR_type == exp->type){
-for(int i=0; i<cnt; i++) cout <<"  ";
-cout<<"duude OR "<<endl;  }
-else cout<<"duude wtf not either!"<<endl;
-
-
-assert(el != NULL, "unhandled null here");
 	// The data structure that link-grammar uses for connector
 	// expressions is totally insane, as witnessed by the loop below.
 	// Anyway: operators are infixed, i.e. are always binary,
 	// with exp->u.l->e being the left hand side, and
 	//      exp->u.l->next->e being the right hand side.
 	// This means that exp->u.l->next->next is always null.
-cnt++;
 	OutList alist;
 	alist.push_back(lg_exp_to_atom(el->e));
-cnt--;
 	el = el->next;
-
-if (el) assert(el->e != NULL, "unhandled crazy null here");
 
 	while (el && exp->type == el->e->type)
 	{
 		el = el->e->u.l;
-assert(el != NULL, "unhandled walk null here");
-assert(el->e != NULL, "unhandled walk more null here");
-cnt++;
 		alist.push_back(lg_exp_to_atom(el->e));
-cnt--;
 		el = el->next;
-// assert(el != NULL, "unhandled walk right null here");
-// assert(el->e != NULL, "unhandled walk more right null here");
 	}
-for(int i=0; i<cnt; i++) cout <<"  ";
-cout<<"duude final "<<endl;
 
-if(el) assert(el->e != NULL, "unhandled final  null here");
 	if (el)
 		alist.push_back(lg_exp_to_atom(el->e));
 
@@ -140,8 +105,6 @@ void Parser::initialize_state()
 	statev.push_back(wall_disj);
 
 	_state = new Link(STATE, statev);
-
-word_disjuncts("XXX");
 }
 
 void viterbi_parse(Dictionary dict, const char * sentence)
@@ -152,6 +115,7 @@ void viterbi_parse(Dictionary dict, const char * sentence)
 	pars.initialize_state();
 cout <<"Hello world!"<<endl;
 
+// pars.word_disjuncts("XXX");
 pars.word_disjuncts("sing");
 }
 
