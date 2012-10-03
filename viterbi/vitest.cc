2d5148b152329f3737f3465800099eed0928b204
Linas Vepstas <linasvepstas@gmail.com>
2012-10-03 01:46:03 +0000

    start working on the unit-test, again
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/link-grammar/trunk@32031 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- viterbi/vitest.cc
+++ viterbi/vitest.cc
@@ -9,12 +9,19 @@
 
 #include <iostream>
 
+#include <link-grammar/link-includes.h>
+#include "read-dict.h"
+
 using namespace std;
 
 // using link_grammar::viterbi;
 
 main()
 {
+	Dictionary dict = dictionary_create_from_utf8("LEFT-WALL: Wd+;");
+
+	print_dictionary_data(dict);
+
 	cout << "hellow world"<<endl;
 
 	exit (0);
