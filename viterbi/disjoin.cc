52943097481a4a1bce37c9269ec75fd51f04579c
Linas Vepstas <linasvepstas@gmail.com>
2012-11-12 02:52:50 +0000

    Flesh out the workings of the disjoin function
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/link-grammar/trunk@32484 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- viterbi/disjoin.cc
+++ viterbi/disjoin.cc
@@ -10,6 +10,7 @@
 /*************************************************************************/
 
 #include "compile.h"
+#include "disjoin.h"
 
 namespace link_grammar {
 namespace viterbi {
@@ -27,21 +28,78 @@ Atom* disjoin(Atom* mixed_form)
 	if ((OR != intype) && (AND != intype))
 		return mixed_form;
 
-	const Link* junct = dynamic_cast<Link*>(mixed_form);
+	Set* junct = dynamic_cast<Set*>(mixed_form);
+	junct = junct->flatten();
+
+	size_t sz = junct->get_arity();
+	OutList new_oset;
+
 	if (OR == intype)
 	{
-		const OutList& oset = junct->get_outgoing_set();
-		size_t sz = junct->get_arity();
-		OutList new_oset;
+		// Just a recursive call, that's all.
 		for(int i=0; i<sz; i++)
 		{
-			Atom* norm = disjoin(oset[i]);
+			Atom* norm = disjoin(junct->get_outgoing_atom(i));
 			new_oset.push_back(norm);
 		}
-		// Link* new_or = Or(new_oset);
 	}
+	else
+	{
+		// If we are here, the outgoing set is a conjunction of atoms.
+		// Search for the first disjunction in that set, and distribute
+		// over it.
+		OutList front;
+		int i;
+		for(i=0; i<sz; i++)
+		{
+			Atom* a = junct->get_outgoing_atom(i);
+			AtomType t = a->get_type();
+			if (OR == t)
+				break;
+			front.push_back(a);
+		}
 
-	return NULL;
+		/* If no disjunctions found, we are done */
+		if (i == sz)
+			return mixed_form;
+
+		Atom *orat = junct->get_outgoing_atom(i);
+
+		OutList rest;
+		for(; i<sz; i++)
+		{
+			Atom* a = junct->get_outgoing_atom(i);
+			rest.push_back(a);
+		}
+
+		Or* orn = dynamic_cast<Or*>(orat);
+		assert(orn, "Bad link type found during disjoin");
+
+		// Distribute over the elements in OR-list
+		sz = orn->get_arity();
+		for (i=0; i<sz; i++)
+		{
+			OutList distrib;
+
+			// Copy the front, without change.
+			size_t jsz = front.size();
+			for (int j=0; j<jsz; j++)
+				distrib.push_back(front[j]);
+
+			// insert one atom.
+			distrib.push_back(orn->get_outgoing_atom(i));
+
+			// Copy the rest.
+			jsz = rest.size();
+			for (int j=0; j<jsz; j++)
+				distrib.push_back(rest[j]);
+
+			And *andy = new And(distrib);
+			new_oset.push_back(andy);
+		}
+	}
+	Or* new_or = new Or(new_oset);
+	return new_or->flatten();
 }
 
 
