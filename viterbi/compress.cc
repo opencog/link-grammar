ea8ee9e104143a36dbf46376e0be646055516b5e
Linas Vepstas <linasvepstas@gmail.com>
2013-03-12 20:51:44 +0000

    viterbi: continue with the merge implementation
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/link-grammar/trunk@32743 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- viterbi/compress.cc
+++ viterbi/compress.cc
@@ -62,8 +62,50 @@ namespace viterbi {
 /// Merge a pair of word-lists, if they are mergable.
 static Seq* merge_wordlists(Seq* wla, Seq* wlb)
 {
-	// size_t 
-	return NULL;
+	size_t sza = wla->get_arity();
+	size_t szb = wlb->get_arity();
+	if (sza != szb)
+		return NULL;
+
+	bool already_merged = false;
+	OutList seq;
+	for (size_t i = 0; i < sza; i++)
+	{
+		Atom* a = wla->get_outgoing_atom(i);
+		Atom* b = wlb->get_outgoing_atom(i);
+
+		if (a->operator==(b))
+		{
+			seq.push_back(a);
+			continue;
+		}
+
+		// If we are here, then the word csets differ.
+		if (already_merged)
+			return NULL;
+		already_merged = true;
+
+		// Check to see if the words are the same.
+		WordCset* wcsa = dynamic_cast<WordCset*>(a);
+		WordCset* wcsb = dynamic_cast<WordCset*>(b);
+		Word* wa = wcsa->get_word();
+		Word* wb = wcsb->get_word();
+
+		// Words differ, its not mergable.
+		if (not wa->operator==(wb))
+			return NULL;
+
+		// OK, all is well, merge the connectors.
+		Atom* csa = wcsa->get_cset();
+		Atom* csb = wcsb->get_cset();
+		Or* dj = new Or(csa, csb);
+
+		// flatten
+		WordCset* merge = new WordCset(wa, dj);
+		seq.push_back(merge);
+	}
+
+	return new Seq(seq);
 }
 
 Set* compress_alternatives(Set* state_alternatives)
