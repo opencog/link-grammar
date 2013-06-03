3efa944255fe9e0308aac47985011266a2988706
Linas Vepstas <linasvepstas@gmail.com>
2013-06-03 22:04:21 +0000

    fix broken is_equal() signature
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/link-grammar/trunk@33133 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- link-grammar/read-dict.c
+++ link-grammar/read-dict.c
@@ -349,10 +349,10 @@ static Boolean link_advance(Dictionary dict)
 /**
  * Returns TRUE if this token is a special token and it is equal to c
  */
-static int is_equal(Dictionary dict, wint_t c)
+static int is_equal(Dictionary dict, char c)
 {
 	return (dict->is_special &&
-	        wctob(c) == dict->token[0] &&
+	        c == dict->token[0] &&
 	        dict->token[1] == '\0');
 }
 
