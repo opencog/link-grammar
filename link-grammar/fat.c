37caef327eed601e178271143986bd140193210d
Linas Vepstas <linasvepstas@gmail.com>
2013-06-18 01:28:58 +0000

    add stubs for fix Mac OSX build break
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/link-grammar/trunk@33175 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ link-grammar/fat.c
@@ -0,0 +1,35 @@
+
+#ifndef USE_FAT_LINKAGES
+
+/* Arghhh. Both Mac OSX and also MSWin fail because these are specified
+ * in link-grammar.def, but are absent in the library.  So hack around
+ * this by creating stub routines.  Don't care about the signature;
+ * they're just junk that won't ever be called, anyway.
+ */
+
+void analyze_fat_linkage (void);
+void analyze_fat_linkage (void) {}
+
+void build_deletable (void);
+void build_deletable (void) {}
+
+void build_effective_dist (void);
+void build_effective_dist (void) {}
+
+void count_set_effective_distance (void);
+void count_set_effective_distance (void) {}
+
+void extract_fat_linkage (void);
+void extract_fat_linkage (void) {}
+
+void parse_options_get_use_fat_links (void);
+void parse_options_get_use_fat_links (void) {}
+
+void parse_options_set_use_fat_links (void);
+void parse_options_set_use_fat_links (void) {}
+
+void set_has_fat_down (void);
+void set_has_fat_down (void) {}
+
+#endif /* USE_FAT_LINKAGES */
+
