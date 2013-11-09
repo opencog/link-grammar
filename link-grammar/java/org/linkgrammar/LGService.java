76fa747996bec58b05d1b7f2797d084bf5cf470a
Linas Vepstas <linasvepstas@gmail.com>
2013-11-09 17:05:29 +0000

    and now .. make use of the new utils
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/link-grammar/trunk@33585 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- link-grammar/java/org/linkgrammar/LGService.java
+++ link-grammar/java/org/linkgrammar/LGService.java
@@ -18,7 +18,6 @@ import java.net.Socket;
 import java.text.CharacterIterator;
 import java.text.SimpleDateFormat;
 import java.text.StringCharacterIterator;
-import java.util.HashMap;
 import java.util.Map;
 // import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -131,18 +130,6 @@ public class LGService
 	{
 		if (verbose)
 			System.out.println("LG " + dateFormatter.format(new java.util.Date()) + " " + s);
-	}
-
-	private static boolean getBool(String name, Map<String, String> msg, boolean def)
-	{
-		String x = msg.get(name);
-		return x == null ? def : Boolean.valueOf(x);
-	}
-
-	private static int getInt(String name, Map<String, String> msg, int def)
-	{
-		String x = msg.get(name);
-		return x == null ? def : Integer.parseInt(x);
 	}
 
 	/**
@@ -375,89 +362,6 @@ public class LGService
 		return result;
 	}
 
-	private static Map<String, String> readMsg(Reader in) throws java.io.IOException
-	{
-		// Read chars from input until input is exhausted, or until
-		// newline is encountered. "length" will be set to the final
-		// length of the input.  The char array buf stores the input;
-		// it is automatically expanded to handle very long inputs.
-		int length = 0;
-		char [] buf = new char[1024];
-		for (int count = in.read(buf, length, buf.length - length);
-		     count > -1;
-		     count = in.read(buf, length, buf.length - length))
-		{
-			length += count;
-			if (length == buf.length)
-			{
-				char [] nbuf = new char[buf.length + 512];
-				System.arraycopy(buf, 0, nbuf, 0, buf.length);
-				buf = nbuf;
-			}
-			if (buf[length-1] == '\n')
-				break;
-		}
-
-		// "result" will contain a map of key-value pairs extracted from
-		// the JSON input. (viz, buf is assumed to contain valid json)
-		Map<String, String> result = new HashMap<String, String>();
-
-		// Note that we expect the JSON part of 'buf' to be in ASCII.
-		// However, the everything after 'text:' might be in UTF-8
-		// That's OK, since the code below just assumes ASCII while
-		// grepping for 'text:' and then lets Java String constructor
-		// deal with the UTF-8. Anyway, it works.
-		boolean gotText = false;
-		int start = -1;
-		int column = -1;
-		for (int offset = 0; offset < length - 1; offset++)
-		{
-			char c = buf[offset];
-			if (start == -1)
-				start = offset;
-			else if (c == ':' && column == -1)
-			{
-				column = offset;
-				String name = new String(buf, start, column - start);
-				name = name.trim();
-				if ("text" == name) gotText = true;
-			}
-
-			// Any commas appearing in the sentence text will fuck this up,
-			// so we treat this as a JSON message, but only until the key
-			// "text" is seen. After that, all commas are assumed to be part
-			// of the text message. (ergo, "text" must the last keyword in
-			// the message.) Note also: after that, the rest might be in UTF-8
-			else if (c == '\0' || (!gotText && c == ','))
-			{
-				if (start == -1 || column == -1)
-					throw new RuntimeException("Malformed message:" + new String(buf, 0, length));
-				String name = new String(buf, start, column - start);
-				String value = new String(buf, column + 1, offset - column - 1);
-				name = name.trim();
-				value = value.trim();
-				result.put(name, value);
-				start = column = -1;
-			}
-		}
-
-		// If we are here, the the last byte wasn't null. This is the
-		// normal exit, I guess ...
-		if (start != -1 && column != -1) {
-			String name = new String(buf, start, column - start);
-			String value = new String(buf, column + 1, length - column - 1);
-			name = name.trim();
-			value = value.trim();
-			result.put(name, value);
-			start = column = -1;
-		}
-		if (start != -1 || column != -1)
-			throw new RuntimeException("Malformed message:"
-				+ new String(buf, 0, length)
-				+ "Did you forget to say \"text:\" at the start of the message?");
-		return result;
-	}
-
 	private static void handleClient(Socket clientSocket)
 	{
 		init();
@@ -467,7 +371,7 @@ public class LGService
 		{
 			trace("Connection accepted from : " + clientSocket.getInetAddress());
 			in = new InputStreamReader(clientSocket.getInputStream());
-			Map<String, String> msg = readMsg(in);
+			Map<String, String> msg = JSONUtils.readMsg(in);
 			if (verbose)
 				trace("Received msg '" + msg + "' from " + clientSocket.getInetAddress());
 			String json = "{}";
@@ -476,13 +380,13 @@ public class LGService
 			else
 			{
 				LGConfig config = new LGConfig();
-				config.setMaxCost(getInt("maxCost", msg, config.getMaxCost()));
-				config.setMaxLinkages(getInt("maxLinkages", msg, config.getMaxLinkages()));
-				config.setMaxParseSeconds(getInt("maxParseSeconds", msg, config.getMaxParseSeconds()));
+				config.setMaxCost(JSONUtils.getInt("maxCost", msg, config.getMaxCost()));
+				config.setMaxLinkages(JSONUtils.getInt("maxLinkages", msg, config.getMaxLinkages()));
+				config.setMaxParseSeconds(JSONUtils.getInt("maxParseSeconds", msg, config.getMaxParseSeconds()));
 				config.setStoreConstituentString(
-					getBool("storeConstituentString", msg, config.isStoreConstituentString()));
+					JSONUtils.getBool("storeConstituentString", msg, config.isStoreConstituentString()));
 				config.setStoreDiagramString(
-					getBool("storeDiagramString", msg, config.isStoreDiagramString()));
+					JSONUtils.getBool("storeDiagramString", msg, config.isStoreDiagramString()));
 				configure(config);
 				String text = msg.get("text");
 				if (text != null && text.trim().length() > 0)
