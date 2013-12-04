b7837d5ffe2c97840d7a3310b223a682f3876937
Linas Vepstas <linasvepstas@gmail.com>
2013-12-04 19:23:38 +0000

    start work on the sql side.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/link-grammar/trunk@33630 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

---
+++ link-grammar/dict-sql/read-sql.c
@@ -0,0 +1,15 @@
+
+/*
+ * read-sql.c
+ *
+ * Read in dictionary from an SQL DB.
+ * Keeping it simple for just right now, and using SQLite.
+ *
+ * Copyright (c) 2013 Linas Vepstas <linasvepstas@gmail.com>
+ */
+
+#ifdef HAVE_SQLITE
+
+#include <sqlite3.h>
+
+#endif /* HAVE_SQLITE */
