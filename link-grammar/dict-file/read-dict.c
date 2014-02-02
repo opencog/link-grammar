46ba4963681387af632366cff30340a5adc9d74e
Linas Vepstas <linasvepstas@gmail.com>
2014-02-02 19:11:58 +0000

    Fix very old bug that leads to .. weirdness.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/link-grammar/trunk@33727 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- link-grammar/dict-file/read-dict.c
+++ link-grammar/dict-file/read-dict.c
@@ -472,41 +472,25 @@ static inline int dict_order(const char *s, const char *t)
 }
 
 /**
- * dict_order_wild() -- order dictionary strings, with wildcard.
+ * dict_order_user() -- order user vs. dictionary string.
  * Assuming that s is a pointer to the search string, and that t is
  * a pointer to a dictionary string, this returns 0 if they match, 
  * returns >0 if s>t, and <0 if s<t.
  *
  * The matching is done as follows.  Walk down the strings until
  * you come to the end of one of them, or until you find unequal
- * characters.  A "*" matches anything.  Otherwise, replace "."
- * by "\0", and take the difference.  This behavior matches that
- * of the function dict_order().
- *
- * Err
- *
- * Note: words in the dictionary itself don't have wild-card
- * chars in them; the wild-card support is here only if you are
- * searching for part of a word.
- *
- * XXX ... except that essentially no one is searching for a fraction
- * of a word, and so this wild-card search is ... unused.  Mostly
- * harmless, I suppose ...
+ * characters.  If the dictionary string contains a dot ".", then
+ * replace "." by "\0", and take the difference.  This behavior
+ * matches that of the function dict_order(), except that here, we
+ * allow user-provided strings with dots in them.  Yes, these are
+ * non-words, probably due to some broken input, but hey ... users
+ * give us broken input all the time...
  */
-static inline int dict_order_wild(const char * s, const char * t)
+static inline int dict_order_user(const char * s, Dict_node * dn)
 {
-	const char *so = s;
-	while((*s != '\0') && (*s == *t)) {s++; t++;}
-
-	if ((*s == '*') || (*t == '*'))
-	{
-		/* Do not allow wild-card as the very first char.
-		 * Basically, allow *.v and *.eq in the English dict.
-		 */
-		if (s != so) return 0;
-	}
-
-	return (((*s == '.')?(0):(*s)) - ((*t == '.')?(0):(*t)));
+	const char * t = dn->string;
+	while ((*s != '\0') && (*s == *t)) {s++; t++;}
+	return ((*s) - ((*t == '.')?(0):(*t)));
 }
 
 /**
@@ -625,7 +609,7 @@ static Dict_node * rdictionary_lookup(Dict_node *llist,
 	int m;
 	Dict_node * dn_new;
 	if (dn == NULL) return llist;
-	m = dict_order_wild(s, dn->string);
+	m = dict_order_user(s, dn);
 	if (m >= 0)
 	{
 		llist = rdictionary_lookup(llist, dn->right, s, match_idiom);
@@ -1820,7 +1804,7 @@ static Boolean find_one_non_idiom_node(Dict_node * p, Dict_node * dn,
 {
 	int m;
 	if (dn == NULL) return FALSE;
-	m = dict_order_wild(s, dn->string);
+	m = dict_order_user(s, dn);
 	if (m <= 0) {
 		if (find_one_non_idiom_node(dn,dn->left, s, parent, to_be_deleted)) return TRUE;
 	}
