c0252b3cc4f755399b3e6414db9aaca156285e6c
Linas Vepstas <linasvepstas@gmail.com>
2014-02-12 16:53:40 +0000

    Fix python example so that russian works.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/link-grammar/trunk@33813 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- bindings/python/example.py
+++ bindings/python/example.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+# -*- coding: utf8 -*-
 #
 # Link Grammar example usage 
 #
@@ -10,8 +11,25 @@ from linkgrammar import Parser, Linkage, ParseOptions, Link
 
 po = ParseOptions()
 
+# English is the default language
 p = Parser()
 linkages = p.parse_sent("This is a test.")
-print "Found ", len(linkages), "linkages"
+print "English: found ", len(linkages), "linkages"
 for linkage in linkages :
     print linkage.diagram
+
+# Russian
+p = Parser(lang = 'ru')
+linkages = p.parse_sent("это большой тест.")
+print "Russian: found ", len(linkages), "linkages"
+for linkage in linkages :
+    print linkage.diagram
+
+# Turkish
+p = Parser(lang = 'tr')
+linkages = p.parse_sent("çok şişman adam geldi")
+print "Turkish: found ", len(linkages), "linkages"
+for linkage in linkages :
+    print linkage.diagram
+
+
