f2048ea605e8ab87fddfdb1782bad79c667aa7fe
Linas Vepstas <linasvepstas@gmail.com>
2014-02-28 20:40:07 +0000

    More URL removal.
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/link-grammar/trunk@33948 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- link-grammar/dict-file/read-regex.h
+++ link-grammar/dict-file/read-regex.h
@@ -2,8 +2,7 @@
 /* Copyright (c) 2005 Sampo Pyysalo                                      */
 /*                                                                       */
 /* Use of the link grammar parsing system is subject to the terms of the */
-/* license set forth in the LICENSE file included with this software,    */
-/* and also available at http://www.link.cs.cmu.edu/link/license.html    */
+/* license set forth in the LICENSE file included with this software.    */
 /* This license allows free redistribution and use in source and binary  */
 /* forms, with or without modification, subject to certain conditions.   */
 /*                                                                       */
