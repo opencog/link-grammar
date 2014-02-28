88fb0853e6f0ae7859c97ac7a152991608503f18
Linas Vepstas <linasvepstas@gmail.com>
2014-02-28 20:29:11 +0000

    Boilerplate changes; remove URL.
    Also, add some missing copyright notices.
    
    
    git-svn-id: svn+ssh://svn.abisource.com/svnroot/link-grammar/trunk@33946 bcba8976-2d24-0410-9c9c-aab3bd5fdfd6

--- link-grammar/extract-links.h
+++ link-grammar/extract-links.h
@@ -4,8 +4,7 @@
 /* All rights reserved                                                   */
 /*                                                                       */
 /* Use of the link grammar parsing system is subject to the terms of the */
-/* license set forth in the LICENSE file included with this software,    */
-/* and also available at http://www.link.cs.cmu.edu/link/license.html    */
+/* license set forth in the LICENSE file included with this softwares.   */
 /* This license allows free redistribution and use in source and binary  */
 /* forms, with or without modification, subject to certain conditions.   */
 /*                                                                       */
