42ee1452651a6f3111d5aa6c7fa51e427dd7de7f
Linas Vepstas <linasvepstas@gmail.com>
2014-12-08 19:03:44 -0600

    create a sort-of-working docker container

--- docker/setup-latest.sh
+++ docker/setup-latest.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
 #
 echo hello world
+wget -r --no-parent -nH --cut-dirs=2 http://www.abisource.com/downloads/link-grammar/current/
+
+tar -xvf current/link-grammar-5*.tar.gz
+cd link-grammar-5*
+mkdir build
+cd build
+../configure
+make -j12
+make install
