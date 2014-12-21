599b9f71e2506bf00f2fceb7e16e69228ceb45a1
Amir Plivatsky <amirpli@gmail.com>
2014-12-21 08:27:04 +0200

    Fix the WGR_NODBGLABEL mode.
    Also add comment about adding a legend in the graph.
    It can be done by displaying a detached node in a corner, with explanation
    labels.
    
    Also fix the comment on non-reentrancy, adding to it the created files.

--- link-grammar/wordgraph.h
+++ link-grammar/wordgraph.h
@@ -5,8 +5,8 @@
 #define WGR_NOPREV         0x04  /* No prev links */
 #define WGR_NOUNSPLIT      0x08  /* No unsplit_word links */
 #define WGR_NODBGLABEL     0x10  /* No debug label addition */
-#define WGR_MAX            0x10 /* Maximum value of WGR_*, for mode[] length */
-#define WGR_MAXDIGITS      2    /* Number of hex digits in WGR_MAX */
+#define WGR_MAX            0x10  /* Maximum value of WGR_*, for mode[] length */
+#define WGR_MAXDIGITS      2     /* Number of hex digits in WGR_MAX */
 void wordgraph_show(Sentence, size_t, const char *);
 #define wordgraph_show(sent, mode) wordgraph_show(sent, mode, #mode)
 #else
