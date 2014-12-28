85df0cb948abdf8b42fb9c5ec581a5b6edd1ac2b
Amir Plivatsky <amirpli@gmail.com>
2014-12-29 01:45:38 +0200

    Minor changes.

--- link-grammar/wordgraph.h
+++ link-grammar/wordgraph.h
@@ -1,14 +1,15 @@
 #ifdef USE_WORDGRAPH_DISPLAY
 /* Wordgraph display representation modes. */
-#define WGR_REG         0x01  /* Ordered sentence words + unsplit link */
+#define WGR_REG         0x01  /* Ordered sentence words + unsplit_word link */
 #define WGR_SUB         0x02  /* Unsplit words as subgraphs */
 #define WGR_NOPREV      0x04  /* No prev links */
 #define WGR_NOUNSPLIT   0x08  /* No unsplit_word links */
 #define WGR_NODBGLABEL  0x10  /* No debug label addition */
-#define WGR_DOTDEBUG    0x20  /* Add hex node numbers, for dot commnds debug */
+#define WGR_DOTDEBUG    0x20  /* Add hex node numbers, for dot commands debug */
 #define WGR_MAX         0x20  /* Maximum value of WGR_*, for pid[] length */
 #define WGR_MAXDIGITS   2     /* Number of hex digits in WGR_MAX */
-void wordgraph_show(Sentence, size_t, const char *);
+
+void wordgraph_show(Sentence, unsigned int, const char *);
 #define wordgraph_show(sent, mode) wordgraph_show(sent, mode, #mode)
 #else
 #define wordgraph_show(sent, mode)
