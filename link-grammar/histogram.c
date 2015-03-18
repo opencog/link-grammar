74e3d5b5b9e3a9bf89dc2f4b9cea0cede170fc5b
Linas Vepstas <linasvepstas@gmail.com>
2015-03-18 17:31:48 -0500

    Optimized slightly for performance

--- link-grammar/histogram.c
+++ link-grammar/histogram.c
@@ -10,6 +10,7 @@
 /*************************************************************************/
 
 #include <math.h>
+#include <stdio.h>
 #include "histogram.h"
 
 /* A histogram distribution of the parse counts. */
@@ -65,12 +66,24 @@ void hist_sum(Count_bin* sum, const Count_bin* a, const Count_bin* b)
 	sum->total = a->total + b->total;
 }
 
+/**
+ * Create a product of histogrammed counts.
+ * Observe that doing so requires a kind-of cross-product to
+ * be performed, thus, a nested double sum.
+ */
 void hist_prod(Count_bin* prod, const Count_bin* a, const Count_bin* b)
 {
-	unsigned int i, j;
+	unsigned int i, k;
 	prod->total = a->total * b->total;
 
+// #define SLOW_BUT_SIMPLE 1
+#ifdef SLOW_BUT_SIMPLE
+	/* The below implements the straight-forward concept of the product.
+	 * Its not quite optimal, because the intialization loop, and the
+	 * if check can be eliminated by re-writing j = k-i.
+	 */
 	for (i = 0; i < NUM_BINS; i++) prod->bin[i] = 0;
+	prod->overrun = 0;
 	for (i = 0; i < NUM_BINS; i++)
 	{
 		for (j = 0; j < NUM_BINS; j++)
@@ -80,32 +93,43 @@ void hist_prod(Count_bin* prod, const Count_bin* a, const Count_bin* b)
 			else
 				prod->overrun += a->bin[i] * b->bin[j];
 		}
+
+		prod->overrun += a->bin[i] * b->overrun;
+		prod->overrun += a->overrun * b->bin[i];
 	}
+	prod->overrun += a->overrun * b->overrun;
+#else
+	/* The below does exactly the same thing as the above, but
+	 * ever so slightly more quickly. Some pointless checks get
+	 * eliminated.
+	 */
+	prod->overrun = 0;
+	for (k = 0; k < NUM_BINS; k++)
+	{
+		prod->bin[k] = 0;
+		for (i = 0; i <= k; i++)
+		{
+			prod->bin[k] += a->bin[i] * b->bin[k-i];
+		}
+		prod->overrun += a->bin[k] * b->overrun;
+		prod->overrun += a->overrun * b->bin[k];
+	}
+	for (k = NUM_BINS; k < 2 * NUM_BINS - 1; k++)
+	{
+		for (i = k - NUM_BINS + 1; i < NUM_BINS; i++)
+		{
+			prod->overrun += a->bin[i] * b->bin[k-i];
+		}
+	}
+	prod->overrun += a->overrun * b->overrun;
+#endif
 }
 
 void hist_muladd(Count_bin* acc, const Count_bin* a, double cost, const Count_bin* b)
 {
-
-	{
-		Count_bin tmp;
-		hist_prod(&tmp, a, b);
-		hist_accum(acc, cost, &tmp);
-	}
-#if 0
-	unsigned int i;
-	unsigned int start = (unsigned int) floor (cost / BIN_WIDTH);
-	acc->total += a->total * b->total;
-	for (i = 0; i < NUM_BINS-start; i++)
-	{
-		s64 subtot = 0;
-		for (j=0; j<start; j++)
-		{
-			subtot += a->bin[i + j] * b->bin[i + j - start];
-		}
-printf("duude muladd %d %llu %llu\n", i, acc->bin[i], a->bin[i-start]);
-		acc->bin[i+start] += subtot;
-	}
-#endif
+	Count_bin tmp;
+	hist_prod(&tmp, a, b);
+	hist_accum(acc, cost, &tmp);
 }
 
 void hist_muladdv(Count_bin* acc, const Count_bin* a, double cost, const Count_bin b)
