56a9c60b1d7b851433edd3f6431d81fbf4141b51
Linas Vepstas <linasvepstas@gmail.com>
2015-03-23 11:16:04 -0500

    Try to debug crazy crash

--- link-grammar/malloc-dbg.c
+++ link-grammar/malloc-dbg.c
@@ -9,11 +9,14 @@
 
 #if !defined(_MSC_VER) && !defined(__MINGW32__)
 #include <execinfo.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
 
 /* ======================================================== */
+void report_mem_dbg(void);
+
 static void *(*old_malloc_hook)(size_t, const void *);
 static void (*old_free_hook)(void *, const void *);
 static void *(*old_realloc_hook)(void *, size_t, const void *);
@@ -21,15 +24,18 @@ static void *(*old_realloc_hook)(void *, size_t, const void *);
 static void my_free_hook(void * mem, const void * caller);
 static void * my_malloc_hook(size_t n_bytes, const void * caller);
 
-#define TBLSZ 366000
+#define TBLSZ 511366000
+// #define TBLSZ 41366000
 typedef struct {
 	void * mem;
 	const void * caller;
-	int cnt;
 	size_t sz;
+	int cnt;
+	bool free;
 } loc_t;
 
-static loc_t loc[TBLSZ];
+// static loc_t loc[TBLSZ];
+static loc_t *loc;
 static int mcnt = 0;
 
 static FILE *fh = NULL;
@@ -41,37 +47,66 @@ static void init_io(void)
 	fh = stdout;
 }
 
+// #define allprt(f, varargs...) fprintf(f, ##varargs)
+#define allprt(f, varargs...) 
 
 static void * my_realloc_hook(void * mem, size_t n_bytes, const void *caller)
 {
+	int i;
+	for (i=0; i<TBLSZ; i++)
+	{
+		if (mem == loc[i].mem)
+		{
+#if TRACK_LEAK
+			loc[i].mem = nm;
+			loc[i].caller = (void *) -1;
+			loc[i].sz = n_bytes;
+#endif
+			allprt(fh, "realloc free %d %p\n", i, mem);
+			if (loc[i].free)
+			{
+				fprintf(fh, "Error realloc of prev free %d %p\n", i, mem);
+			}
+			loc[i].free = true;
+			break;
+		}
+	}
+	if (TBLSZ == i)
+	{
+		fprintf(fh, "Error: realloc address not previously alloed!\n");
+	}
+
 	__realloc_hook = old_realloc_hook;
 	void * nm = realloc(mem, n_bytes);
 	old_realloc_hook = __realloc_hook;
 	__realloc_hook = my_realloc_hook;
 
-	int i;
+#if TRACK_LEAK
+	if (i < TBLSZ) return nm;
+#endif
+	if (i < TBLSZ)
+	{
+		loc[i].mem = nm;
+		loc[i].caller = caller;
+		loc[i].sz = n_bytes;
+		mcnt++;
+		loc[i].cnt = mcnt;
+		loc[i].free = false;
+		allprt(fh, "realloc reuse alloc %d %p\n", i, nm);
+		return nm;
+	}
+
 	for (i=0; i<TBLSZ; i++)
 	{
-		if(mem == loc[i].mem)
+		if ((void *) -1 == loc[i].mem)
 		{
 			loc[i].mem = nm;
-			loc[i].caller = (void *) -1;
-			loc[i].sz = n_bytes;
-			/* loc[i].cnt = -1;
-			   fprintf(fh, "realloc %d %p\n", i, mem); */
-			break;
-		}
-	}
-	if (i <= TBLSZ) return nm;
-	for (i=0; i<TBLSZ; i++)
-	{
-		if((void *) -1 == loc[i].mem)
-		{
-			loc[i].mem = mem;
 			loc[i].sz = n_bytes;
 			loc[i].caller = caller;
-			loc[i].cnt = -2;
-			/* fprintf(fh, "realloc %d %p\n", i, mem); */
+			mcnt ++;
+			loc[i].cnt = mcnt;
+			loc[i].free = false;
+			allprt(fh, "realloc new alloc %d %p\n", i, nm);
 			break;
 		}
 	}
@@ -91,15 +126,33 @@ static void * my_malloc_hook(size_t n_bytes, const void * caller)
 	int i;
 	for (i=0; i<TBLSZ; i++)
 	{
-		if((void *) -1 == loc[i].mem)
+		if (mem == loc[i].mem)
 		{
 			loc[i].mem = mem;
 			loc[i].caller = caller;
-			/* fprintf(fh,"malloc %d %p\n", i, mem); */
+			allprt(fh,"malloc reuse %d %p\n", i, mem);
+			loc[i].sz = n_bytes;
 			mcnt ++;
 			loc[i].cnt = mcnt;
-			loc[i].sz = n_bytes;
+			loc[i].free = false;
 			break;
+		}
+	}
+	if (i == TBLSZ)
+	{
+		for (i=0; i<TBLSZ; i++)
+		{
+			if ((void *) -1 == loc[i].mem)
+			{
+				loc[i].mem = mem;
+				loc[i].caller = caller;
+				allprt(fh,"malloc fresh %d %p\n", i, mem);
+				loc[i].sz = n_bytes;
+				mcnt ++;
+				loc[i].cnt = mcnt;
+				loc[i].free = false;
+				break;
+			}
 		}
 	}
 
@@ -107,6 +160,7 @@ static void * my_malloc_hook(size_t n_bytes, const void * caller)
 	if (i == TBLSZ)
 	{
 		size_t totsz=0;
+#if REPORT
 		for (i=0; i<TBLSZ; i++)
 		{
 		  /* size_t off = loc[i].caller - (void *) my_malloc_hook; */
@@ -114,7 +168,8 @@ static void * my_malloc_hook(size_t n_bytes, const void * caller)
 			       i, loc[i].caller, loc[i].sz, loc[i].cnt);
 			totsz += loc[i].sz;
 		}
-		fprintf (fh, "Total Size=%x\n", totsz);
+#endif
+		fprintf (fh, "Total Size=%zu\n", totsz);
 		*((int *)0) = 1; /* Force a crash, pop into debugger. */
 		exit(1);
 	}
@@ -132,24 +187,32 @@ static void my_free_hook(void * mem, const void * caller)
 	int i;
 	for (i=0; i<TBLSZ; i++)
 	{
-		if(mem == loc[i].mem)
+		if (mem == loc[i].mem)
 		{
+#if TRACK_LEAK
 			loc[i].mem = (void *) -1;
 			loc[i].caller = 0;
 			loc[i].sz = 0;
 			loc[i].cnt = -3;
-			/* fprintf(fh, "free %d %p\n", i, mem); */
+#endif
+			allprt(fh, "free %d %p\n", i, mem);
+			if (loc[i].free)
+			{
+				fprintf(fh, "Error double free %d %p\n", i, mem);
+			}
+			loc[i].free = true;
 			break;
 		}
 	}
 
 	/* Its a double-free if the mem address wasn't found! */
+#if TRACK_LEAK
 	if (TBLSZ == i)
 	{
 		int i;
 #define BTSZ 10
 		void * btbuf[BTSZ];
-		fprintf(fh, "Double free of mem location %p\n", mem);
+		fprintf(fh, "Mem location %p is not alloced!\n", mem);
 		int nt = backtrace (btbuf, BTSZ);
 		char ** bt = backtrace_symbols(btbuf, nt);
 		for (i=0; i<nt; i++)
@@ -160,6 +223,7 @@ static void my_free_hook(void * mem, const void * caller)
 		*((int *)0) = 1; /* Force a crash, pop into debugger. */
 		exit(1);
 	}
+#endif
 
 	__malloc_hook = my_malloc_hook;
 
@@ -181,11 +245,21 @@ void report_mem_dbg(void)
 			totsz += loc[i].sz;
 		}
 	}
-	fprintf (fh, "tbl slots used = %d alloc mem=%d\n", cnt, totsz);
+	fprintf (fh, "tbl slots used = %d alloc mem=%zu\n", cnt, totsz);
 }
 
+void my_init_hook(void);
 void my_init_hook(void)
 {
+	static bool is_init = false;
+	if (is_init) return;
+	is_init = true;
+
+	printf("init malloc trace hook loc_t size=%d mbytes=%d\n",
+		sizeof(loc_t), (TBLSZ * sizeof(loc_t))/(1024*1024));
+
+	loc = (loc_t*) malloc (TBLSZ * sizeof(loc_t));
+
 	old_malloc_hook = __malloc_hook;
 	__malloc_hook = my_malloc_hook;
 
@@ -200,10 +274,11 @@ void my_init_hook(void)
 	{
 		loc[i].mem = (void *) -1;
 		loc[i].caller = 0;
-		loc[i].cnt = 0;
 		loc[i].sz = 0;
+		loc[i].cnt = 0;
+		loc[i].free = true;
 	}
 }
 
-void (*__malloc_initialize_hook)(void) = my_init_hook;
+void (*__MALLOC_HOOK_VOLATILE __malloc_initialize_hook)(void) = my_init_hook;
 #endif /*_MSC_VER && __MINGW32__*/
