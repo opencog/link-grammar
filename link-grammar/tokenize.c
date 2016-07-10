f7b64ccbec76cb6d47ce541fcb90dcf23b92a1fa
Amir Plivatsky <amirpli@gmail.com>
2016-07-11 01:29:21 +0300

    tokenize.c: Use debug_level() instead of verbosity
    ... so it will take a full benefit of the debug print control recently
    added.

--- link-grammar/tokenize.c
+++ link-grammar/tokenize.c
@@ -1521,7 +1521,7 @@ static bool guess_misspelled_word(Sentence sent, Gword *unsplit_word,
 	/* Else, ask the spell-checker for alternate spellings
 	 * and see if these are in the dict. */
 	n = spellcheck_suggest(dict->spell_checker, &alternates, word);
-	if (3 < verbosity)
+	if (debug_level(+D_SW))
 	{
 		printf("Info: guess_misspelled_word() spellcheck_suggest for %s:%s\n",
 		       word, (0 == n) ? " (nothing)" : "");
@@ -2994,7 +2994,7 @@ bool flatten_wordgraph(Sentence sent, Parse_Options opts)
 
 	free(wp_new);
 	lgdebug(+D_FW, "sent->length %zu\n", sent->length);
-	if (D_FW <= opts->verbosity)
+	if (debug_level(D_SW))
 		print_sentence_word_alternatives(sent, true, NULL, NULL);
 
 	return !error_encountered;
