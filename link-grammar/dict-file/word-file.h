7a193d8267c2d21a9236c8950a8f5e896535d5cf
ampli <amirpli@gmail.com>
2016-09-18 07:55:52 +0300

    Word_file_struct: Add a comment that "changed" is unused.

--- link-grammar/dict-file/word-file.h
+++ link-grammar/dict-file/word-file.h
@@ -19,7 +19,7 @@ struct Word_file_struct
 {
 	Word_file * next;
 	const char *file;    /* the file name */
-	bool changed;        /* TRUE if this file has been changed */
+	bool changed;        /* TRUE if this file has been changed (XXX unused) */
 };
 
 void free_Word_file(Word_file * wf);
