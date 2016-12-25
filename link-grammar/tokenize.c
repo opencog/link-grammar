b10861b78e0d57ff017babea955be1599fbfe43f
ampli <amirpli@gmail.com>
2016-12-25 03:37:05 +0200

    do_count(): Account for "optional" words
    The trick is to allow them to remain out of the linkage (i.e. be "null
    words" - words with no links) while not incrementing the null_count.
    
    Also here:
    1. flatten_wordgraph() doesn't issue any more an empty-word when
    a slot is optional. Instead, it just marks it as optional.
    2. remove_empty_words() now needs to remove the optional words
    which happen to be "null words".
    
    FIXME?
    Rename remove_empty_words() to remove_optional_null_words().
    Rename empty_word_encountered to optional_word_encountered.

--- link-grammar/tokenize.c
+++ link-grammar/tokenize.c
@@ -2810,7 +2810,6 @@ bool flatten_wordgraph(Sentence sent, Parse_Options opts)
 	{
 		Word *wa_word; /* A word-array word (for the parsing stage) */
 		const Gword *unsplit_word;
-		bool empty_word_encountered;
 
 		assert(NULL != wp_new, "pathpos word queue is empty");
 		wp_old = wp_new;
@@ -2843,7 +2842,6 @@ bool flatten_wordgraph(Sentence sent, Parse_Options opts)
 			}
 		}
 
-		empty_word_encountered = false;
 		/* Generate the X-nodes. */
 		for (wpp_old = wp_old; NULL != wpp_old->word; wpp_old++)
 		{
@@ -2852,20 +2850,13 @@ bool flatten_wordgraph(Sentence sent, Parse_Options opts)
 
 			if (wpp_old->same_word)
 			{
-				/* We haven't advanced to the next wordgraph word, so its X-node has
-				 * already been generated in a previous word of the word array.
-				 * Generate an empty word if one has not already been generated.
-				 */
-				if (!empty_word_encountered)
-				{
-					/* ??? Should we check it earlier? */
-					if (!sent->dict->empty_word_defined)
-						prt_error("Error: %s must be defined!\n", EMPTY_WORD_DOT);
-
-					if (!determine_word_expressions(sent, empty_word(), &ZZZ_added))
-						error_encountered = true;
-					empty_word_encountered = true;
-				}
+				/* We haven't advanced to the next wordgraph word, so its X-node
+				 * has already been generated in a previous word of the word
+				 * array.  This means we are in a longer alternative which has
+				 * "extra" words that may not have links, and this is one of
+				 * them.  Mark it as "optional", so we consider that while
+				 * parsing, and then remove it in case it doesn't have links. */
+				sent->word[sent->length - 1].optional = true;
 			}
 			else
 			{
