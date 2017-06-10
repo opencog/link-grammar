107499f7c4eee09a0f23179c5bc19db140330298
ampli <amirpli@gmail.com>
2017-06-10 22:19:22 +0300

    dict-structures.h: Clean whitespace

--- link-grammar/dict-common/dict-structures.h
+++ link-grammar/dict-common/dict-structures.h
@@ -25,7 +25,7 @@ typedef struct Exp_struct Exp;
 typedef struct E_list_struct E_list;
 typedef struct Word_file_struct Word_file;
 
-/** 
+/**
  * Types of Exp_struct structures
  */
 typedef enum
@@ -35,7 +35,7 @@ typedef enum
 	CONNECTOR_type
 } Exp_type;
 
-/** 
+/**
  * The E_list and Exp structures defined below comprise the expression
  * trees that are stored in the dictionary.  The expression has a type
  * (AND, OR or TERMINAL).  If it is not a terminal it has a list
@@ -43,35 +43,35 @@ typedef enum
  */
 struct Exp_struct
 {
-    Exp * next;    /* Used only for mem management,for freeing */
-    Exp_type type; /* One of three types: AND, OR, or connector. */
-    char dir;      /* '-' means to the left, '+' means to right (for connector) */
-    bool multi;    /* TRUE if a multi-connector (for connector)  */
-    union {
-        E_list * l;           /* Only needed for non-terminals */
-        const char * string;  /* Only needed if it's a connector */
-    } u;
-    double cost;   /* The cost of using this expression.
-                      Only used for non-terminals */
+	Exp * next;    /* Used only for mem management,for freeing */
+	Exp_type type; /* One of three types: AND, OR, or connector. */
+	char dir;      /* '-' means to the left, '+' means to right (for connector) */
+	bool multi;    /* TRUE if a multi-connector (for connector)  */
+	union {
+		E_list * l;           /* Only needed for non-terminals */
+		const char * string;  /* Only needed if it's a connector */
+	} u;
+	double cost;   /* The cost of using this expression.
+	                  Only used for non-terminals */
 };
 
 struct E_list_struct
 {
-    E_list * next;
-    Exp * e;
+	E_list * next;
+	Exp * e;
 };
 
-/** 
+/**
  * The dictionary is stored as a binary tree comprised of the following
  * nodes.  A list of these (via right pointers) is used to return
  * the result of a dictionary lookup.
  */
 struct Dict_node_struct
 {
-    const char * string;  /* The word itself */
-    Word_file * file;     /* The file the word came from (NULL if dict file) */
-    Exp       * exp;
-    Dict_node *left, *right;
+	const char * string;  /* The word itself */
+	Word_file * file;     /* The file the word came from (NULL if dict file) */
+	Exp       * exp;
+	Dict_node *left, *right;
 };
 
 LINK_END_DECLS
