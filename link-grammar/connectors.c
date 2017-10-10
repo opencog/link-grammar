3157204d007d916c047d074a91c3ca9e182ed41f
ampli <amirpli@gmail.com>
2017-10-10 18:59:39 +0300

    Remove tableNext initialization
    It is zeroed by zero_connector_table().
    New connectors are inserted in front of the list.

--- link-grammar/connectors.c
+++ link-grammar/connectors.c
@@ -62,7 +62,6 @@ Connector * connector_new(const condesc_t *desc, Parse_Options opts)
 	c->desc = desc;
 	c->nearest_word = 0;
 	c->multi = false;
-	c->tableNext = NULL;
 	set_connector_length_limit(c, opts);
 	//assert(0 != c->length_limit, "Connector_new(): Zero length_limit");
 
