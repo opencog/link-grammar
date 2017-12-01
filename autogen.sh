ad64e6339f6fca89472624c3452ea6eb74128d95
ampli <amirpli@gmail.com>
2017-12-01 20:31:41 +0200

    OSX: Use glibtoolie instead of libtoolize
    (libtoolize is usually installed on OSX as glibtoolize.)

--- autogen.sh
+++ autogen.sh
@@ -23,17 +23,22 @@ if test ! -d `aclocal --print-ac-dir 2>> autogen.err`; then
   exit 1
 fi
 
-libtoolize --force --copy || {
-    echo "error: libtoolize failed"
-    exit 1
-}
-
 # Produce aclocal.m4, so autoconf gets the automake macros it needs
 # 
 case `uname` in
     CYGWIN*)
         include_dir='-I m4' # Needed for Cygwin only.
+        ;;
+    Darwin)
+        [ "$LIBTOOLIZE" = "" ] && LIBTOOLIZE=glibtoolize
+        ;;
 esac
+
+    ${LIBTOOLIZE:=libtoolize} --force --copy || {
+    echo "error: libtoolize failed"
+    exit 1
+}
+
 echo "Creating aclocal.m4: aclocal $include_dir $ACLOCAL_FLAGS"
 
 aclocal $include_dir $ACLOCAL_FLAGS 2>> autogen.err
