8092a56a794e5c5a5b3aaebeafe383c9bfc351e3
ampli <amirpli@gmail.com>
2018-04-05 01:58:54 +0300

    Windows: Add extended version print
    Due to limitation in the build system of Windows, it is not possible to
    directly capture the compilation definitions.
    
    So instead of directly define compilation definitions, add a user macro
    CFLAGS in the MSVC-common property sheet.
    
    Also, add a user macro DEFS in the LGlib-features property sheet,
    for the library feature definitions (like USE_WORDGRAPH_DISPLAY=1).
    
    Since it is not possible to externally inspect $(PreprocessorDefinitions)
    but it is still a good idea to show if the DEBUG definition has been
    used, $(Configuration) is inspected for "Debug", and if found, a dummy
    CPPFLAGS is set to "DEBUG;".
    
    (The GOTO is used because using a () block somehow messes the registry
    variable.)
    
     # Please enter the commit message for your changes. Lines starting

--- msvc14/confvar.bat
+++ msvc14/confvar.bat
@@ -2,6 +2,10 @@
 %= Filter - replace the *VERSION atoconf variables according to the ones   =%
 %= set in configure.ac.                                                    =%
 
+setlocal
+set configuration=%1
+if "%Configuration%"=="Debug" ( set Debug=DEBUG; ) else ( set Debug= )
+
 setlocal DisableDelayedExpansion
 mkdir Temp 2>nul
 
@@ -14,6 +18,18 @@ findstr "^LINK_.*_VERSION=" %conf% >  %tmp%
 findstr "^VERSION="         %conf% >> %tmp%
 for /F "tokens=*" %%i in (%tmp%) do set %%i
 del %tmp%
+
+	if "%Configuration%"=="" GOTO skipos
+	%= Get OS name                                                         =%
+	set "osreg=HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+	for /f "tokens=3*" %%i in ('reg query "%osreg%" /v ProductName') do (
+			set "win_os= %%i %%j"
+	)
+	%= Get OS version                                                      =%
+	for /f "tokens=3*" %%i in ('ver') do (
+			set "win_ver= %%i %%j"
+	)
+:skipos
 
 %= Process the ".in" file, preserving blank lines and exlamation marks =%
 for /f "tokens=1* delims=]" %%i in ('find /v /n ""') do (
@@ -28,6 +44,13 @@ for /f "tokens=1* delims=]" %%i in ('find /v /n ""') do (
 		set "line=!line:$LINK_MAJOR_VERSION=%LINK_MAJOR_VERSION%!"
 		set "line=!line:$LINK_MINOR_VERSION=%LINK_MINOR_VERSION%!"
 		set "line=!line:$LINK_MICRO_VERSION=%LINK_MICRO_VERSION%!"
+
+		if NOT "%Configuration%"=="" (
+			set "line=!line:@HOST_OS@=%win_os% %win_ver%!"
+			set "line=!line:@LG_DEFS@=%DEFS%!"
+			set "line=!line:@CPPFLAGS@=%Debug%!"
+			set "line=!line:@CFLAGS@=%CFLAGS%!"
+		)
 
 		echo(!line!
 		endlocal
