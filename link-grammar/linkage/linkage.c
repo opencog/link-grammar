6313b1b181ba8e9eaa1b5046df20fe57516cb226
ampli <amirpli@gmail.com>
2018-10-08 20:29:33 +0300

    Just forget discarded words in chosen_words[]
    Comment out The code that keeps discarded words at unreachable
    chosen_words[] - it is not clear why it was ever needed.
    (Keep it for reference in case the reason is discovered.)

--- link-grammar/linkage/linkage.c
+++ link-grammar/linkage/linkage.c
@@ -682,9 +682,9 @@ void compute_chosen_words(Sentence sent, Linkage linkage, Parse_Options opts)
 		if (chosen_words[i] &&
 		    (chosen_words[i][0] || (!HIDE_MORPHO || show_word[i])))
 		{
-			const char *cwtmp = linkage->word[j];
+			//const char *cwtmp = linkage->word[j];
 			linkage->word[j] = chosen_words[i];
-			chosen_words[i] = cwtmp;
+			//chosen_words[i] = cwtmp;
 			remap[i] = j;
 			j++;
 		}
