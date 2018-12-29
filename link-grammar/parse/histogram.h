ec8d9dfbab27ac8ae33e5e754cf9cdaec0628dfd
ampli <amirpli@gmail.com>
2018-12-29 09:30:46 +0200

    histogram.h: Remove MSVC6 support
    (Not needed now.)

--- link-grammar/parse/histogram.h
+++ link-grammar/parse/histogram.h
@@ -12,15 +12,8 @@
 #ifndef _HISTOGRAM_H_
 #define _HISTOGRAM_H_
 
-#ifndef _MSC_VER
 typedef long long s64; /* signed 64-bit integer, even on 32-bit cpus */
 #define PARSE_NUM_OVERFLOW (1LL<<24)
-#else
-/* Microsoft Visual C Version 6 doesn't support long long. */
-typedef signed __int64 s64; /* signed 64-bit integer, even on 32-bit cpus */
-#define PARSE_NUM_OVERFLOW (((s64)1)<<24)
-#endif
-
 
 /*
  * Count Histogramming is currently not required for anything, and the
