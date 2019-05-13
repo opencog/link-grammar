93a8681304872680dd40b87eb6e861918232411d
ampli <amirpli@gmail.com>
2019-05-13 13:42:53 +0300

    classic_parse(): Add number of parses in the time printout

--- link-grammar/parse/parse.c
+++ link-grammar/parse/parse.c
@@ -350,7 +350,8 @@ void classic_parse(Sentence sent, Parse_Options opts)
 		total = (total < 0) ? INT_MAX : total;
 
 		sent->num_linkages_found = (int) total;
-		print_time(opts, "Counted parses");
+		print_time(opts, "Counted parses (%lld w/%zu null%s)", hist_total(&hist),
+		           sent->null_count, (sent->null_count != 1) ? "s" : "");
 
 		if (verbosity >= D_USER_INFO)
 		{
