c738932d662dac64252328b32d53af65872a9e66
ampli <amirpli@gmail.com>
2019-07-23 22:30:31 +0300

    Shorten timing message (-v=2) for encoding connectors

--- link-grammar/parse/parse.c
+++ link-grammar/parse/parse.c
@@ -281,7 +281,7 @@ void classic_parse(Sentence sent, Parse_Options opts)
 		saved_memblock = save_disjuncts(sent, ts_pruning);
 	}
 
-	print_time(opts, "Encode connectors for pruning%s%s",
+	print_time(opts, "Encode for pruning%s%s",
 	           (NULL == ts_pruning->tracon_list) ? " (skipped)" : "",
 	           (one_step_parse) ? " (one-step)" : "");
 
@@ -311,7 +311,7 @@ void classic_parse(Sentence sent, Parse_Options opts)
 				needed_prune_level++; /* In case we need to prune again. */
 
 			Tracon_sharing *ts_parsing = pack_sentence_for_parsing(sent);
-			print_time(opts, "Encode connectors for parsing");
+			print_time(opts, "Encode for parsing");
 			if (NULL != ts_parsing)
 			{
 				sent->dc_memblock = ts_parsing->memblock;
