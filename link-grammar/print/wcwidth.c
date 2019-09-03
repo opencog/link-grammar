c14b49d35516b9cabdf2808947a016f0ec64c928
ampli <amirpli@gmail.com>
2019-09-04 01:12:36 +0300

    bisearch(): Change first arg to int in case wchar_t is unsigned
    This prevents a compiler warning in that case.

--- link-grammar/print/wcwidth.c
+++ link-grammar/print/wcwidth.c
@@ -71,7 +71,11 @@ struct interval {
 };
 
 /* auxiliary function for binary search in interval table */
-static bool bisearch(wchar_t ucs, const struct interval *table, int max)
+/* NOTE: The actual parameter is wchar_t, which may be unsigned on
+ * some architectures (e.g. ARM). An (int) formal parameter is used to
+ * prevent a compiler warning in that case (((unsigned int)wchar_t) is
+ * always < INT_MAX) so its value is preserved). */
+static bool bisearch(int ucs, const struct interval *table, int max)
 {
   int min = 0;
   int mid;
