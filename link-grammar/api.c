e0d0a2ca587654ca386ec0219dd4d5a517804082
ampli <amirpli@gmail.com>
2021-02-16 20:55:03 +0200

    Set min_len_encoding=0 in generation mode
    This is a must fro any reasonable performance.

--- link-grammar/api.c
+++ link-grammar/api.c
@@ -451,7 +451,10 @@ Sentence sentence_create(const char *input_string, Dictionary dict)
 	/* Set the minimum length for tracon sharing.
 	 * In that case a tracon list is produced for the pruning step,
 	 * and disjunct/connector packing is done too. */
-	sent->min_len_encoding = SENTENCE_MIN_LENGTH_TRAILING_HASH;
+	if (IS_GENERATION(dict))
+		sent->min_len_encoding = 0;
+	else
+		sent->min_len_encoding = SENTENCE_MIN_LENGTH_TRAILING_HASH;
 	const char *min_len_encoding = test_enabled("min-len-encoding");
 	if (NULL != min_len_encoding)
 		sent->min_len_encoding = atoi(min_len_encoding+1);
