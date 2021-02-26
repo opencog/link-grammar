a39c5d1046f69d7cc2f1e5d4a90fa5d6f24cf638
Linas Vepstas <linasvepstas@gmail.com>
2021-02-26 11:09:38 -0600

    Try to get the generator to compile

--- link-parser/link-generator.c
+++ link-parser/link-generator.c
@@ -5,8 +5,19 @@
  * February 2021
  */
 
+#include <stddef.h>
+
+#include "../link-grammar/link-includes.h"
+
 int main (int argc, char* argv[])
 {
+	const char     *language = NULL;
+	Dictionary      dict;
+
+	if ((argc > 1) && (argv[1][0] != '-')) {
+		/* The dictionary is the first argument if it doesn't begin with "-" */
+		language = argv[1];
+	}
 
 	return 0;
 }
