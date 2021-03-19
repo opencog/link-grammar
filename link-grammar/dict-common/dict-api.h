2b6a8bbc50493427f30b9b57fd5e5b25298a842f
ampli <amirpli@gmail.com>
2021-03-19 23:58:35 +0200

    dict-api.h: Add variable names to make it more like link-include.h

--- link-grammar/dict-common/dict-api.h
+++ link-grammar/dict-common/dict-api.h
@@ -32,11 +32,11 @@ LINK_BEGIN_DECLS
  ***********************************************************************/
 
 link_public_api(Dict_node *)
-	dictionary_lookup_list(const Dictionary, const char *);
+	dictionary_lookup_list(const Dictionary dict, const char *word);
 link_public_api(Dict_node *)
-	dictionary_lookup_wild(const Dictionary, const char *);
+	dictionary_lookup_wild(const Dictionary dict, const char *word);
 link_public_api(void)
-	free_lookup_list(const Dictionary, Dict_node *);
+	free_lookup_list(const Dictionary dict, Dict_node * n);
 
 /**********************************************************************
  *
@@ -45,18 +45,18 @@ link_public_api(void)
  ***********************************************************************/
 
 link_public_api(const Category *)
-	dictionary_get_categories(const Dictionary);
+	dictionary_get_categories(const Dictionary dict);
 
 link_public_api(const Category_cost *)
-	linkage_get_categories(const Linkage, WordIdx);
+	linkage_get_categories(const Linkage linkage, WordIdx w);
 
 
 /* Return true if word can be found. */
 link_public_api(bool)
-	dictionary_word_is_known(const Dictionary, const char *);
+	dictionary_word_is_known(const Dictionary dict, const char *word);
 
 /* This was exported and used by mistake! */
-bool boolean_dictionary_lookup(const Dictionary, const char *);
+bool boolean_dictionary_lookup(const Dictionary dict, const char *word);
 
 /* XXX the below probably does not belong ...  ?? */
 Dict_node * insert_dict(Dictionary dict, Dict_node * n, Dict_node * newnode);
