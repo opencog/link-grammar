ca67c49c0595b5d0ed9b0de3c332cbb1d01274de
Linas Vepstas <linasvepstas@gmail.com>
2021-03-30 12:16:12 -0500

    Prepare for an odometer walk over sentences

--- link-parser/generator-utilities.c
+++ link-parser/generator-utilities.c
@@ -12,20 +12,18 @@
 
 const char *select_word(const Category *catlist,
                         const Category_cost *cc,
-                        unsigned int disjunct_num_categories,
-								WordIdx w)
+                        WordIdx w,
+                        unsigned int disjunct_category_idx)
 {
 	/* Select a disjunct category. */
-	unsigned int r = (unsigned int)rand();
-	unsigned int disjunct_category_idx = r % disjunct_num_categories;
 	unsigned int category_num = cc[disjunct_category_idx].num;
-	lgdebug(5, "Word %zu: r=%08x category %d/%u \"%u\";", w, r,
-	        disjunct_category_idx, disjunct_num_categories, category_num);
+	lgdebug(5, "Word %zu: category %d \"%u\";", w,
+	        disjunct_category_idx, category_num);
 	category_num--; /* Categories starts with 1 but Category is 0 based. */
 	unsigned int num_words = catlist[category_num].num_words;
 
 	/* Select a dictionary word from the selected disjunct category. */
-	r = (unsigned int)rand();
+	unsigned int r = (unsigned int)rand();
 	unsigned int dict_word_idx = r % num_words;
 	const char *word = catlist[category_num].word[dict_word_idx];
 	lgdebug(5, " r=%08x word %d/%u \"%s\"\n",
@@ -45,13 +43,15 @@ static const char *cond_subscript(const char *ow, bool leave_subscript)
 	return w;
 }
 
-void print_sentence(const Category* catlist,
-                    Linkage linkage, size_t nwords, const char** words,
-                    bool subscript, bool explode)
+static void print_sent(const Category* catlist,
+                       Linkage linkage, size_t nwords, const char** words,
+                       bool subscript,
+                       const Category_cost** cclist,
+                       unsigned int* cc_select)
 {
 	for(WordIdx w = 0; w < nwords; w++)
 	{
-		const Category_cost *cc = linkage_get_categories(linkage, w);
+		const Category_cost *cc = cclist[w];
 		if (cc == NULL)
 		{
 			// When is cc NULL? When does this ever happen?
@@ -60,17 +60,84 @@ void print_sentence(const Category* catlist,
 			continue;
 		}
 
-		unsigned int dj_num_cats;
-		for (dj_num_cats = 0; cc[dj_num_cats].num != 0; dj_num_cats++)
-			;
-		assert(dj_num_cats != 0, "Bad disjunct!");
-
-		const char *word = select_word(catlist, cc, dj_num_cats, w);
+		unsigned int dj_cat_idx = cc_select[w];
+		const char *word = select_word(catlist, cc, w, dj_cat_idx);
 		printf("%s", cond_subscript(word, subscript));
 		if (w < nwords-1) printf(" ");
 	}
 
 	printf("\n");
+}
+
+static void print_sent_all(const Category* catlist,
+                           Linkage linkage, size_t nwords, const char** words,
+                           bool subscript)
+{
+	const Category_cost* cclist[nwords];
+	unsigned int cclen[nwords];
+	unsigned int cc_select[nwords];
+
+	for(WordIdx w = 0; w < nwords; w++)
+	{
+		const Category_cost* cc = linkage_get_categories(linkage, w);
+		cclist[w] = cc;
+
+		unsigned int dj_num_cats = 0;
+		cclen[w] = 0;
+		if (NULL != cclist[w]) /* when is it ever null ??? */
+		{
+			while (cc[dj_num_cats].num != 0) dj_num_cats++;
+			assert(dj_num_cats != 0, "Bad disjunct!");
+
+			cclen[w] = dj_num_cats;
+		}
+
+		/* Pick one at random. */
+		unsigned int r = (unsigned int)rand();
+		cc_select[w] = r % dj_num_cats;
+	}
+
+	print_sent(catlist, linkage, nwords, words, subscript,
+	           cclist, cc_select);
+}
+
+void print_sentence(const Category* catlist,
+                    Linkage linkage, size_t nwords, const char** words,
+                    bool subscript, bool explode)
+{
+	if (explode)
+	{
+		print_sent_all(catlist, linkage, nwords, words, subscript);
+		return;
+	}
+
+	/* Otherwise, just select one word at random */
+	const Category_cost* cclist[nwords];
+	unsigned int cclen[nwords];
+	unsigned int cc_select[nwords];
+
+	for(WordIdx w = 0; w < nwords; w++)
+	{
+		const Category_cost* cc = linkage_get_categories(linkage, w);
+		cclist[w] = cc;
+
+		unsigned int dj_num_cats = 0;
+		cclen[w] = 0;
+		if (NULL != cclist[w]) /* when is it ever null ??? */
+		{
+			while (cc[dj_num_cats].num != 0) dj_num_cats++;
+			assert(dj_num_cats != 0, "Bad disjunct!");
+
+			cclen[w] = dj_num_cats;
+		}
+
+		/* Pick one at random. */
+		unsigned int r = (unsigned int)rand();
+		cc_select[w] = r % dj_num_cats;
+	}
+
+	print_sent(catlist, linkage, nwords, words, subscript,
+	           cclist, cc_select);
 }
 
 /* ------------------------------------------------------------ */
