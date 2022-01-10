bc9fa0895b33fea62f6463354d58a88b9e98ffee
ampli <amirpli@gmail.com>
2022-01-10 18:17:18 +0200

    strtodC: Use strtod() also for NetBSD
    FIXME: Use a string-to-float conversion code that always uses ".".

--- link-grammar/utilities.c
+++ link-grammar/utilities.c
@@ -759,7 +759,7 @@ bool strtodC(const char *s, float *r)
 {
 	char *err;
 
-#if defined(HAVE_LOCALE_T) && !defined(__sun__)
+#if defined(HAVE_LOCALE_T) && !defined(__sun__) && !defined(__OpenBSD__)
 	double val = strtod_l(s, &err, get_C_LC_NUMERIC());
 #else
 	/* dictionary_setup_locale() invokes setlocale(LC_NUMERIC, "C") */
