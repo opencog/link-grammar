8f7737195f606248d0dbb03778872eb462609bed
Arm <arm@Arms-MacBook-Pro.local>
2022-02-16 17:09:16 +0700

    Compiled prepositions

--- data/step01_list_multipos.py
+++ data/step01_list_multipos.py
@@ -58,6 +58,30 @@ def print_clusters(clstbl, wordtbl):
 
 ############################################################
 
+def print_postbl(clstbl, wordtbl, fields, fieldmap):
+    clsidx = 0
+    clslist = [x for (i, x) in sorted([(len(cls), cls) for cls in clstbl], reverse=True)]
+    print('\t' + '\t'.join(fields))
+    emptyrow = '\t'.join([f if f == '|' else '' for f in fields])
+    print('\t' + emptyrow)
+
+    for cls in clslist:
+        tempcls = [fieldmap[x[1]] for x in cls]
+        # input(f'tempcls = {tempcls}')
+        for word in clstbl[cls]:
+            print(word, end='')
+            for field in fields:
+                if field == '|':
+                    print('\t|', end='')
+                elif field in tempcls:
+                    print('\t1', end='')
+                else:
+                    print('\t', end='')
+            print()
+        print('\t' + emptyrow)
+
+############################################################
+
 def main():
     wordtbl = {}
     for fname in sys.argv[1:]:
@@ -65,7 +89,63 @@ def main():
         load_dict(wordtbl, fname)
         sys.stderr.write(f' done\n'); sys.stderr.flush()
     clstbl = cluster_postags(wordtbl)
-    print_clusters(clstbl, wordtbl)
+    # print_clusters(clstbl, wordtbl)
+
+    fields = [
+        '|',
+        'V', 'VA', '|',
+        'N', 'NA', 'NP', '|',
+        'PR', 'RL', 'SM', '|',
+        'J', 'JL', '|',
+        'PAA', 'PAN', 'PAV', 'PNA', 'PNN', 'PNV', 'PVA', 'PVN', 'PVV', '|',
+        'R', 'RA', 'RC', 'RQ', 'X', 'NGL', 'NGR', 'PS', 'PA', 'IJ', '|',
+        'NUM', 'NUMV', 'NUMO', 'ORD', '|',
+        'CLN', 'CLV', 'QFL', 'QFR', '|',
+        'CD', 'CN', 'CV', '|',
+    ]
+    fieldmap = {
+        'words.adj.common': 'J',
+        'words.adj.lhs': 'JL',
+        'words.adv.attrmod': 'RA',
+        'words.adv.cohesive': 'RC',
+        'words.adv.common': 'R',
+        'words.adv.qf': 'RQ',
+        'words.aux.common': 'X',
+        'words.cl.noun': 'CLN',
+        'words.cl.verb': 'CLV',
+        'words.conj.discont': 'CD',
+        'words.conj.noun': 'CN',
+        'words.conj.verb': 'CV',
+        'words.intj.common': 'IJ',
+        'words.n.attribute': 'NA',
+        'words.n.common': 'N',
+        'words.n.prefix': 'NP',
+        'words.neg.lhs': 'NGL',
+        'words.neg.rhs': 'NGR',
+        'words.num.common': 'NUM',
+        'words.num.movable': 'NUMV',
+        'words.num.ordinal': 'NUMO',
+        'words.ordmark': 'ORD',
+        'words.part.common': 'PA',
+        'words.passmark': 'PS',
+        'words.prep-n.n': 'PNN',
+        'words.prep-n.v': 'PNV',
+        'words.prep-v.n': 'PVN',
+        'words.prep-v.nv': 'PVA',
+        'words.prep-v.v': 'PVV',
+        'words.prep.lw': 'PLW',
+        'words.prep.n': 'PAN',
+        'words.prep.nv': 'PAA',
+        'words.prep.v': 'PAV',
+        'words.pro.common': 'PR',
+        'words.qf.lhs': 'QFL',
+        'words.qf.rhs': 'QFR',
+        'words.relpro.common': 'RL',
+        'words.sclmark': 'SM',
+        'words.v.attribute': 'VA',
+        'words.v.common': 'V',
+    }
+    print_postbl(clstbl, wordtbl, fields, fieldmap)
 
 ############################################################
 
