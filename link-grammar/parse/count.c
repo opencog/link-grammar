9f4b01ae321365e73163c79216d721df0350cb69
ampli <amirpli@gmail.com>
2022-06-07 02:50:28 +0300

    do_count(): Check for l_bnr/r_bnl unconditionally

--- link-grammar/parse/count.c
+++ link-grammar/parse/count.c
@@ -1205,18 +1205,15 @@ static Count_bin do_count(
 
 				if (!leftpcount && !rightpcount) continue;
 
-				if (!(leftpcount && rightpcount))
+				if (leftpcount)
 				{
-					if (leftpcount)
-					{
-						/* Evaluate using the left match, but not the right. */
-						COUNT(l_bnr, do_count(ctxt, w, rw, d->right, re, rnull_cnt));
-					}
-					else if (le == NULL)
-					{
-						/* Evaluate using the right match, but not the left. */
-						COUNT(r_bnl, do_count(ctxt, lw, w, le, d->left, lnull_cnt));
-					}
+					/* Evaluate using the left match, but not the right. */
+					COUNT(l_bnr, do_count(ctxt, w, rw, d->right, re, rnull_cnt));
+				}
+				else if (le == NULL)
+				{
+					/* Evaluate using the right match, but not the left. */
+					COUNT(r_bnl, do_count(ctxt, lw, w, le, d->left, lnull_cnt));
 				}
 
 #define CACHE_COUNT(c, how_to_count, do_count) \
