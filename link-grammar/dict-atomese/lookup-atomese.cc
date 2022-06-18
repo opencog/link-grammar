c71980b8917c2122623659b13a0441e448a1e0e3
Linas Vepstas <linasvepstas@gmail.com>
2022-06-18 13:30:45 -0500

    Start talking to the cogserver

--- link-grammar/dict-atomese/lookup-atomese.cc
+++ link-grammar/dict-atomese/lookup-atomese.cc
@@ -41,9 +41,7 @@ void as_open(Dictionary dict, const char* url)
 
 	local->stnp->open();
 
-	// XXX FIXME. Right now, this is lying about the connection,
-	// because open() does not actually try to connect. This needs
-	// fixing ... sometime later.
+	// XXX FIXME -- if we cannot connect, then should hard-fail.
 	if (local->stnp->connected())
 		printf("Connected to %s\n", url);
 	else
@@ -64,9 +62,19 @@ void as_close(Dictionary dict)
 bool as_lookup(Dictionary dict, const char *s)
 {
 	Local* local = (Local*) (dict->as_server);
-printf("duuude called as_lookup for %s\n", s);
+printf("duuude called as_lookup for >>%s<<\n", s);
 	Handle wrd = local->asp->get_node(WORD_NODE, s);
-	return nullptr != wrd;
+	if (nullptr == wrd)
+		wrd = local->asp->add_node(WORD_NODE, s);
+
+	// Are there any Sections in the local atomspace?
+	size_t nsects = wrd->getIncomingSetSizeByType(SECTION);
+	if (0 == nsects)
+		local->stnp->fetch_incoming_by_type(wrd, SECTION);
+
+	nsects = wrd->getIncomingSetSizeByType(SECTION);
+printf("duuude as_lookup for >>%s<< sects=%lu\n", s, nsects);
+	return 0 != nsects;
 }
 
 Dict_node * as_lookup_list(Dictionary dict, const char *s)
