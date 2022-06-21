9d357c5968ab66079578e20b4e7f49e145e08bc8
Linas Vepstas <linasvepstas@gmail.com>
2022-06-21 12:52:46 -0500

    Fixes for issue #1272
    Issue #1272 describes a build problem on the Mac.

--- bindings/js/build_packages.sh
+++ bindings/js/build_packages.sh
@@ -8,6 +8,7 @@ pushd "$(dirname "$0")"
 
 # Compile LLVM bitcode
 pushd ../..
+make clean
 emconfigure ./configure --disable-editline --disable-sat-solver --disable-java-bindings --disable-python-bindings
 emmake make
 popd
