d68d05f4f95fb4e46ede043f8d353005164ff831
Linas Vepstas <linasvepstas@gmail.com>
2022-07-11 13:13:28 -0500

    Provide a more commplex demo dictionary

--- data/demo-atomese/atomese-dict.scm
+++ data/demo-atomese/atomese-dict.scm
@@ -8,6 +8,10 @@
 ;
 ; It is intended that this file can be read in using the
 ; FileStorageNode provided by the AtomSpace.
+;
+; ---------------------------------------------------------------------
+; The four sections below allow one and only one phrase to be parsed:
+; "level playing field".
 
 (Section
 	(Word "###LEFT-WALL###")
@@ -30,3 +34,54 @@
 	(Word "field")
 	(ConnectorSeq
 		(Connector (Word "playing") (ConnectorDir "-"))))
+
+; ---------------------------------------------------------------------
+; The data below allows a collection of very similar sentences
+; to be parsed. They all have the form "Mary saw a bird", with
+; a choice of subjects, objects, verbs and determiners possible,
+; as listed below.
+
+(Section
+	(Word "###LEFT-WALL###")
+	(ConnectorSeq
+		(Connector (WordClass "person") (ConnectorDir "+"))))
+
+(Member (Word "Mary") (WordClass "person"))
+(Member (Word "John") (WordClass "person"))
+(Member (Word "Olga") (WordClass "person"))
+(Member (Word "Sasha") (WordClass "person"))
+
+(Section
+	(WordClass "person")
+	(ConnectorSeq
+		(Connector (Word "###LEFT-WALL###") (ConnectorDir "-"))
+		(Connector (WordClass "verb") (ConnectorDir "+"))))
+
+(Member (Word "saw") (WordClass "verb"))
+(Member (Word "heard") (WordClass "verb"))
+
+(Section
+	(WordClass "verb")
+	(ConnectorSeq
+		(Connector (WordClass "person") (ConnectorDir "-"))
+		(Connector (WordClass "animal") (ConnectorDir "+"))))
+
+(Member (Word "bird") (WordClass "animal"))
+(Member (Word "cat") (WordClass "animal"))
+(Member (Word "dog") (WordClass "animal"))
+
+(Section
+	(WordClass "animal")
+	(ConnectorSeq
+		(Connector (WordClass "determiner") (ConnectorDir "-"))
+		(Connector (WordClass "verb") (ConnectorDir "-"))))
+
+(Member (Word "the") (WordClass "determiner"))
+(Member (Word "a") (WordClass "determiner"))
+(Member (Word "this") (WordClass "determiner"))
+(Member (Word "that") (WordClass "determiner"))
+
+(Section
+	(WordClass "determiner")
+	(ConnectorSeq
+		(Connector (WordClass "animal") (ConnectorDir "+"))))
