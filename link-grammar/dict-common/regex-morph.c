4dd57abd2ae4aff67b0b356869f546e2d401c9c4
ampli <amirpli@gmail.com>
2022-08-08 13:47:04 +0300

    regex-morph.c: Add match offset support

--- link-grammar/dict-common/regex-morph.c
+++ link-grammar/dict-common/regex-morph.c
@@ -47,19 +47,40 @@ LINK_BEGIN_DECLS
 #include "dict-common/regex-morph.h"
 LINK_END_DECLS
 
+/* ovector     - returned start and end offsets.
+ * re_code     - compiled regex.
+ * re_md       - match data.
+ */
+
 #if HAVE_PCRE2_H
 typedef struct {
 	pcre2_code *re_code;
 	pcre2_match_data* re_md;
-} regex_t;
+} reg_info;
+#endif
+
+#if HAVE_REGEX_H
+typedef struct {
+	regex_t re_code;
+	unsigned int Nre_md;
+	regmatch_t re_md[];
+} reg_info;
+#endif
+
+#if USE_CXXREGEX
+typedef struct {
+	std::regex *re_code;
+	std::cmatch re_md;
+} reg_info;
 #endif
 
 #define ERRBUFFLEN 120
 
-/* Define reg_comp(), reg_match() and reg_free() for the selected
- * regex implementation. */
+/* Define reg_comp(), reg_match() and reg_free(), and reg_span()
+ * for the selected regex implementation. */
 
 /* ========================================================================= */
+
 #if HAVE_REGEX_H
 /**
  * Find an upper limit to the number of capture groups in the pattern.
@@ -88,15 +109,21 @@ static bool reg_comp(Regex_node *rn)
 #define REG_GNU 0
 #endif
 
-	regex_t *re = (regex_t *)malloc(sizeof(regex_t));
+	const bool nosub = (rn->capture_group < 0);
+	const unsigned int Novector = nosub ? 0 : (max_capture_groups(rn) + 1);
+	int options = REG_EXTENDED|REG_ENHANCED|REG_GNU|(nosub ? REG_NOSUB : 0);
 
-	int rc =
-		regcomp(re, rn->pattern, REG_NOSUB|REG_EXTENDED|REG_ENHANCED|REG_GNU);
+	reg_info *re = malloc(sizeof(reg_info) + sizeof(regmatch_t) * 2 * Novector);
+	memset(re, 0, sizeof(reg_info)); /* No need to initialize re_md. */
 
-	if (rc)
+	re->Nre_md = Novector;
+
+	int rc = regcomp(&re->re_code, rn->pattern, options);
+
+	if (rc != 0)
 	{
 		char errbuf[ERRBUFFLEN];
-		regerror(rc, re, errbuf, ERRBUFFLEN);
+		regerror(rc, &re->re_code, errbuf, ERRBUFFLEN);
 		prt_error("Error: Failed to compile regex: \"%s\" (pattern \"%s\"): %s "
 		          "(code %d)\n", rn->name, rn->pattern, errbuf, rc);
 		free(re);
@@ -109,24 +136,42 @@ static bool reg_comp(Regex_node *rn)
 
 static bool reg_match(const char *s, const Regex_node *rn)
 {
-	int rc = regexec((regex_t *)rn->re, s, 0, NULL, /*eflags*/0);
+	reg_info *reinfo = (reg_info *)rn->re;
+	int nmatch = (rn->capture_group < 0) ? 0 : reinfo->Nre_md;
+	int rc = regexec(&reinfo->re_code, s, nmatch, reinfo->re_md, /*eflags*/0);
 
 	if (rc == REG_NOMATCH) return false;
 	if (rc == 0) return true;
 
 	/* We have an error. */
 	char errbuf[ERRBUFFLEN];
-	regerror(rc, (regex_t *)rn->re, errbuf, ERRBUFFLEN);
+	regerror(rc, &reinfo->re_code, errbuf, ERRBUFFLEN);
 	prt_error("Error: Regex matching error: \"%s\" (pattern \"%s\"): %s "
 	          "(code %d)\n", rn->name, rn->pattern, errbuf, rc);
 	return false;
 }
 
+static void reg_span(Regex_node *rn)
+{
+	int cgn = rn->capture_group;
+	reg_info *reinfo = rn->re;
+
+	if (unlikely(cgn > rn->capture_group))
+	{
+		rn->ovector[0] = rn->ovector[1] = -1;
+	}
+	else
+	{
+		rn->ovector[0] = reinfo->re_md[cgn].rm_so;
+		rn->ovector[1] = reinfo->re_md[cgn].rm_eo;
+	}
+}
+
 static void reg_free(Regex_node *rn)
 {
-	regex_t *re = (regex_t *)rn->re;
-	regfree(re);
-	free(re);
+	regex_t *re_code = &((reg_info *)rn->re)->re_code;
+	regfree(re_code);
+	free(rn->re);
 }
 #endif // HAVE_REGEX_H
 
@@ -134,15 +179,18 @@ static void reg_free(Regex_node *rn)
 #if HAVE_PCRE2_H
 static bool reg_comp(Regex_node *rn)
 {
-	regex_t *re = rn->re = malloc(sizeof(regex_t));
+	reg_info *re = rn->re = malloc(sizeof(reg_info));
 	PCRE2_SIZE erroffset;
 	int rc;
 
+	const bool nosub = (rn->capture_group < 0);
+	uint32_t options = PCRE2_UTF|PCRE2_UCP| (nosub ? PCRE2_NO_AUTO_CAPTURE : 0);
+
 	re->re_code = pcre2_compile((PCRE2_SPTR)rn->pattern, PCRE2_ZERO_TERMINATED,
-	                            PCRE2_UTF|PCRE2_UCP, &rc, &erroffset, NULL);
+	                            options, &rc, &erroffset, NULL);
 	if (re->re_code != NULL)
 	{
-		re->re_md = pcre2_match_data_create(0, NULL);
+		re->re_md = pcre2_match_data_create_from_pattern(re->re_code, NULL);
 		if (re->re_md == NULL)
 		{
 			prt_error("Error: pcre2_match_data_create failed\n");
@@ -164,11 +212,11 @@ static bool reg_comp(Regex_node *rn)
 
 static int reg_match(const char *s, const Regex_node *rn)
 {
-	regex_t *re = rn->re;
+	reg_info *reinfo = rn->re;
 
-	int rc = pcre2_match(re->re_code, (PCRE2_SPTR)s,
+	int rc = pcre2_match(reinfo->re_code, (PCRE2_SPTR)s,
 	                     PCRE2_ZERO_TERMINATED, /*startoffset*/0,
-	                     PCRE2_NO_UTF_CHECK, re->re_md, NULL);
+	                     PCRE2_NO_UTF_CHECK, reinfo->re_md, NULL);
 	if (rc == PCRE2_ERROR_NOMATCH) return false;
 	if (rc >= 0) return true;
 
@@ -180,9 +228,26 @@ static int reg_match(const char *s, const Regex_node *rn)
 	return false;
 }
 
+static void reg_span(Regex_node *rn)
+{
+	int cgn = rn->capture_group;
+	reg_info *reinfo = rn->re;
+	PCRE2_SIZE *ovector = pcre2_get_ovector_pointer(reinfo->re_md);
+
+	if (unlikely(cgn >= (int)pcre2_get_ovector_count(reinfo->re_md)))
+	{
+		rn->ovector[0] = rn->ovector[1] = -1;
+	}
+	else
+	{
+		rn->ovector[0] = ovector[2*cgn];
+		rn->ovector[1] = ovector[2*cgn + 1];
+	}
+}
+
 static void reg_free(Regex_node *rn)
 {
-	regex_t *re = rn->re;
+	reg_info *re = rn->re;
 
 	pcre2_match_data_free(re->re_md);
 	pcre2_code_free(re->re_code);
@@ -194,9 +259,11 @@ static void reg_free(Regex_node *rn)
 #if USE_CXXREGEX
 static bool reg_comp(Regex_node *rn)
 {
+	rn->re = new reg_info;
+
 	try
 	{
-		rn->re = new std::regex(rn->pattern);
+		((reg_info *)rn->re)->re_code = new std::regex(rn->pattern);
 	}
 	catch (const std::regex_error& e)
 	{
@@ -210,11 +277,13 @@ static bool reg_comp(Regex_node *rn)
 
 static bool reg_match(const char *s, const Regex_node *rn)
 {
+	/* "nosub" not used, as no time difference found w/o using reinfo->re_md. */
 	bool match;
+	reg_info *reinfo = (reg_info *)rn->re;
 
 	try
 	{
-		match = std::regex_search(s, *(std::regex*)rn->re);
+		match = std::regex_search(s, reinfo->re_md, *reinfo->re_code);
 	}
 	catch (const std::regex_error& e)
 	{
@@ -226,9 +295,26 @@ static bool reg_match(const char *s, const Regex_node *rn)
 	return match;
 }
 
+static void reg_span(Regex_node *rn)
+{
+	int cgn = rn->capture_group;
+	std::cmatch re_md = ((reg_info *)rn->re)->re_md;
+
+	if (unlikely(cgn >= (int)re_md.size()))
+	{
+		rn->ovector[0] = rn->ovector[1] = -1;
+	}
+	else
+	{
+		rn->ovector[0] = re_md.position(cgn);
+		rn->ovector[1] = rn->ovector[0] + re_md.length(cgn);
+	}
+}
+
 static void reg_free(Regex_node *rn)
 {
-	delete (std::regex *)rn->re;
+	delete ((reg_info *)rn->re)->re_code;
+	delete (reg_info *)rn->re;
 }
 #endif // USE_CXXREGEX
 
