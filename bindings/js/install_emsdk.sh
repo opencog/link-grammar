424063f54fc5c9ffbf5088ae1b56dc273d9f52a4
Linas Vepstas <linasvepstas@gmail.com>
2023-01-18 13:51:28 -0600

    Set the emsdk version to "latest"

--- bindings/js/install_emsdk.sh
+++ bindings/js/install_emsdk.sh
@@ -6,8 +6,8 @@ pushd "$(dirname "$0")"
 git clone https://github.com/emscripten-core/emsdk.git || true
 pushd emsdk
 git pull
-./emsdk install
-./emsdk activate
+./emsdk install latest
+./emsdk activate latest
 popd
 
 popd
