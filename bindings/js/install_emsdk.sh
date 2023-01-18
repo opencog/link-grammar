0940e2a7a161d83b5a2d6673b397f1969e73a34a
Linas Vepstas <linasvepstas@gmail.com>
2023-01-18 13:45:44 -0600

    Remove explicit version specs.

--- bindings/js/install_emsdk.sh
+++ bindings/js/install_emsdk.sh
@@ -6,8 +6,8 @@ pushd "$(dirname "$0")"
 git clone https://github.com/emscripten-core/emsdk.git || true
 pushd emsdk
 git pull
-./emsdk install 1.38.41
-./emsdk activate 1.38.41
+./emsdk install
+./emsdk activate
 popd
 
 popd
