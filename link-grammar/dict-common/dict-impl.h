a2265d932c4375f4274704f75edb91435b807a50
Linas Vepstas <linasvepstas@gmail.com>
2023-01-23 13:33:38 -0600

    Remove commented out code; reorganize

--- link-grammar/dict-common/dict-impl.h
+++ link-grammar/dict-common/dict-impl.h
@@ -1,24 +1,18 @@
 
-#include "link-includes.h"
-#include "dict-common/dict-common.h"        // For Afdict_class
-#include "utilities.h"
-
+/**
+ * Private functions for dictionary maintainers. Coude outside of
+ * this directory should not call these functions.
+ */
 #ifndef _DICT_IMPL_H_
 #define _DICT_IMPL_H_
 
-// Already declared in link-includes.h
-// const char * linkgrammar_get_dict_locale(Dictionary dict);
-// const char * linkgrammar_get_version(void);
-// const char * linkgrammar_get_dict_version(Dictionary dict);
+#include "link-includes.h"
+#include "dict-common/dict-common.h"        // For Afdict_class
 
 void dictionary_setup_locale(Dictionary dict);
 bool dictionary_setup_defines(Dictionary dict);
 void afclass_init(Dictionary dict);
 bool afdict_init(Dictionary dict);
 void affix_list_add(Dictionary afdict, Afdict_class *, const char *);
-
-#ifdef __MINGW32__
-int callGetLocaleInfoEx(LPCWSTR, LCTYPE, LPWSTR, int);
-#endif /* __MINGW32__ */
 
 #endif /* _DICT_IMPL_H_ */
