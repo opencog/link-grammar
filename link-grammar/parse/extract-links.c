7d9461157382dd22086f21ff25f0173618946077
Linas Vepstas <linasvepstas@gmail.com>
2023-06-18 21:44:58 -0500

    Revert part of #1473

--- link-grammar/parse/extract-links.c
+++ link-grammar/parse/extract-links.c
@@ -298,7 +298,7 @@ void free_extractor(extractor_t * pex)
 
 	xfree((void *) pex, sizeof(extractor_t));
 
-#if defined __GLIBC__ && defined HAVE_MALLOC_TRIM
+#if defined __GLIBC__
 	// malloc_trim() is a gnu extension.  An alternative would be
 	// to call madvise(MADV_DONTNEED) but this is more complicated.
 	if (trim) malloc_trim(0);
