8a0fa56e1afd24c63c4a29a630b55f56c5556c33
ampli <amirpli@gmail.com>
2024-05-18 16:45:47 +0300

    autogen.sh: Remove automake
    It is invoked by autoreconf.

--- autogen.sh
+++ autogen.sh
@@ -16,17 +16,6 @@ fi
 # Update the m4 macros
 autoreconf -fvi
 
-# Produce all the `GNUmakefile.in's and create neat missing things
-# like `install-sh', etc.
-#
-echo "automake --add-missing --copy --foreign"
-
-automake --add-missing --copy --foreign 2>> autogen.err || {
-    echo ""
-    echo "* * * warning: possible errors while running automake - check autogen.err"
-    echo ""
-}
-
 # If there's a config.cache file, we may need to delete it.
 # If we have an existing configure script, save a copy for comparison.
 if [ -f config.cache ] && [ -f configure ]; then
