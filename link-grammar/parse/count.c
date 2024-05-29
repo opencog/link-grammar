870393811b0902e1301aa8c3e48f904a4402c585
ampli <amirpli@gmail.com>
2024-05-30 02:39:02 +0300

    table_lookup(): Remove redundant check in assert()

--- link-grammar/parse/count.c
+++ link-grammar/parse/count.c
@@ -576,9 +576,9 @@ static Count_bin table_store(count_context_t *ctxt,
 		Count_bin *e = table_lookup(ctxt, lw, rw, le, re, null_count, NULL);
 		if (e != NULL)
 		{
-			assert((e == NULL) || (hist_total(&c) == hist_total(e)),
 			       "Inconsistent count for w(%d,%d) tracon_id(%d,%d)",
 			       lw, rw, l_id, r_id);
+			assert((hist_total(&c) == hist_total(e)),
 			return c;
 		}
 
